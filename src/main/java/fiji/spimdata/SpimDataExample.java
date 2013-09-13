c942fbcb2deb4b7286663f455b0fe3ab6d31e948
Stephan Preibisch <stephan.preibisch@gmx.de>
2013-09-13 13:22:10 -0400

    added support for the hasBeads tag by extending ViewSetup

--- src/main/java/fiji/spimdata/SpimDataExample.java
+++ src/main/java/fiji/spimdata/SpimDataExample.java
@@ -14,11 +14,20 @@ import javax.xml.transform.stream.StreamResult;
 import mpicbg.spim.data.SpimData;
 import mpicbg.spim.data.XmlHelpers;
 import mpicbg.spim.data.XmlIoSpimData;
+import mpicbg.spim.data.registration.XmlIoViewRegistrations;
 import mpicbg.spim.data.sequence.TimePoint;
 import mpicbg.spim.data.sequence.ViewDescription;
 import mpicbg.spim.data.sequence.ViewSetup;
+import mpicbg.spim.data.sequence.XmlIoImgLoader;
+import mpicbg.spim.data.sequence.XmlIoMissingViews;
+import mpicbg.spim.data.sequence.XmlIoSequenceDescription;
+import mpicbg.spim.data.sequence.XmlIoTimePoints;
+import mpicbg.spim.data.sequence.XmlIoViewSetups;
 
 import org.w3c.dom.Document;
+
+import fiji.spimdata.sequence.ViewSetupBeads;
+import fiji.spimdata.sequence.XmlIoViewSetupsBeads;
 
 public class SpimDataExample
 {
@@ -26,8 +35,13 @@ public class SpimDataExample
 	{
 		// load SpimData from xml file
 		final String xmlFilename = "/Users/preibischs/workspace/fiji/src-plugins/SPIM_Registration/src/main/resources/example_fromdialog.xml";
-		final XmlIoSpimData< TimePoint, ViewSetup > io = XmlIoSpimData.createDefault();
-		final SpimData< TimePoint, ViewSetup > spimData = io.load( xmlFilename );	
+		
+		//final XmlIoSpimData< TimePoint, ViewSetupBeads > io = XmlIoSpimData.createDefault();
+		final XmlIoSequenceDescription< TimePoint, ViewSetupBeads > seqDesc = 
+				new XmlIoSequenceDescription< TimePoint, ViewSetupBeads >( new XmlIoTimePoints(), new XmlIoViewSetupsBeads(), new XmlIoMissingViews(), new XmlIoImgLoader() );
+		final XmlIoSpimData< TimePoint, ViewSetupBeads > io = 
+				new XmlIoSpimData< TimePoint, ViewSetupBeads >( seqDesc, new XmlIoViewRegistrations() );
+		final SpimData< TimePoint, ViewSetupBeads > spimData = io.load( xmlFilename );	
 		
 		// save SpimData to xml file
 		io.save( spimData, "example_new2.xml" );
