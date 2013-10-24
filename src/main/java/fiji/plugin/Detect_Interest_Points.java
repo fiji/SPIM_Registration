8aa504c9de59d38b4526ecfd78f49ae52247c5b5
Stephan Preibisch <stephan.preibisch@gmx.de>
2013-10-24 09:44:58 -0400

    moved methods for getting all channels, angles and illuminations to spim_data

--- src/main/java/fiji/plugin/Detect_Interest_Points.java
+++ src/main/java/fiji/plugin/Detect_Interest_Points.java
@@ -1,16 +1,13 @@
 package fiji.plugin;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-
-import mpicbg.spim.data.sequence.ViewSetup;
-import net.imglib2.util.Util;
-
 import fiji.plugin.LoadParseQueryXML.XMLParseResult;
-import fiji.spimdata.SpimDataBeads;
 import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
+
+import java.util.ArrayList;
+
+import mpicbg.spim.data.sequence.Angle;
+import mpicbg.spim.data.sequence.Channel;
 
 public class Detect_Interest_Points implements PlugIn
 {
@@ -22,76 +19,19 @@ public class Detect_Interest_Points implements PlugIn
 		if ( result == null )
 			return;
 		
-		final int[] channels = getChannels( result.getData() );
+		final ArrayList< Channel > channels = result.getData().getSequenceDescription().getAllChannels( );
 		
-		System.out.println( Util.printCoordinates( getAngles( result.getData() ) ) );
+		for ( final Angle a : result.getData().getSequenceDescription().getAllAngles() )
+			System.out.println( a.getName() );
 		
 		final GenericDialog gd = new GenericDialog( "Detect Interest Points" );
 		
-		gd.addMessage("Channels: " + channels.length );
+		gd.addMessage("Channels: " + channels.size() );
 		
 		gd.showDialog();
 		
 		//gd.addChoice( "Select_type_of_registration", beadRegistration, beadRegistration[ defaultBeadRegistration ] );		
 		//gd.addChoice( "Select_type_of_detection", beadDetectionType, beadDetectionType[ defaultBeadDetectionType ] );
-	}
-	
-	
-	public int[] getChannels( final SpimDataBeads spimData )
-	{
-		final List< ViewSetup > viewSetups = spimData.getSequenceDescription().getViewSetups();
-		final HashSet< Integer > set = new HashSet< Integer >();
-		
-		for ( final ViewSetup v : viewSetups )
-			set.add( v.getChannel().getId() );
-		
-		final int[] channels = new int[ set.size() ];
-
-		int i = 0;
-		for ( final int c : set )
-			channels[ i++ ] = c;
-
-		Arrays.sort( channels );
-		
-		return channels;
-	}
-
-	public int[] getAngles( final SpimDataBeads spimData )
-	{
-		final List< ViewSetup > viewSetups = spimData.getSequenceDescription().getViewSetups();
-		final HashSet< Integer > set = new HashSet< Integer >();
-		
-		for ( final ViewSetup v : viewSetups )
-			set.add( v.getAngle().getId() );
-		
-		final int[] angles = new int[ set.size() ];
-
-		int i = 0;
-		for ( final int c : set )
-			angles[ i++ ] = c;
-
-		Arrays.sort( angles );
-		
-		return angles;
-	}
-
-	public int[] getNumIlluminationDirections( final SpimDataBeads spimData )
-	{
-		final List< ViewSetup > viewSetups = spimData.getSequenceDescription().getViewSetups();
-		final HashSet< Integer > set = new HashSet< Integer >();
-		
-		for ( final ViewSetup v : viewSetups )
-			set.add( v.getIllumination().getId() );
-		
-		final int[] illuminations = new int[ set.size() ];
-
-		int i = 0;
-		for ( final int c : set )
-			illuminations[ i++ ] = c;
-
-		Arrays.sort( illuminations );
-		
-		return illuminations;
 	}
 	
 	public static void main( final String[] args )
