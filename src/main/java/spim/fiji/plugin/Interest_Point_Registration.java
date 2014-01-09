d0775d73587ae692bb6a0f496bbf972c540710bb
Stephan Preibisch <stephan.preibisch@gmx.de>
2014-01-09 14:56:01 -0500

    started working on registration

--- src/main/java/spim/fiji/plugin/Interest_Point_Registration.java
+++ src/main/java/spim/fiji/plugin/Interest_Point_Registration.java
@@ -1,5 +1,28 @@
 package spim.fiji.plugin;
 
-public class Interest_Point_Registration {
+import spim.fiji.plugin.LoadParseQueryXML.XMLParseResult;
+import ij.plugin.PlugIn;
 
+/**
+ *
+ * @author Stephan Preibisch (stephan.preibisch@gmx.de)
+ *
+ */
+public class Interest_Point_Registration implements PlugIn
+{
+
+	@Override
+	public void run( final String arg )
+	{
+		final XMLParseResult result = new LoadParseQueryXML().queryXML( false );
+
+		if ( result == null )
+			return;
+
+	}
+
+	public static void main( String[] args )
+	{
+		new Interest_Point_Registration().run( null );
+	}
 }
