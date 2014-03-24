b75c5354077ade046ae7731fe1c2a844914d8ffa
Stephan Preibisch <stephan.preibisch@gmx.de>
2014-03-23 21:59:53 -0400

    added the reference timepoint registration, not tested yet

---
+++ src/main/java/spim/fiji/plugin/interestpointregistration/optimizationtypes/ReferenceTimepointRegistration.java
@@ -0,0 +1,156 @@
+package spim.fiji.plugin.interestpointregistration.optimizationtypes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+import mpicbg.spim.data.sequence.Angle;
+import mpicbg.spim.data.sequence.Illumination;
+import mpicbg.spim.data.sequence.TimePoint;
+import mpicbg.spim.data.sequence.ViewDescription;
+import mpicbg.spim.data.sequence.ViewId;
+import mpicbg.spim.data.sequence.ViewSetup;
+import spim.fiji.plugin.interestpointregistration.ChannelInterestPointList;
+import spim.fiji.plugin.interestpointregistration.ChannelInterestPointListPair;
+import spim.fiji.plugin.interestpointregistration.ChannelProcess;
+import spim.fiji.spimdata.SpimData2;
+
+public class ReferenceTimepointRegistration extends GlobalOptimizationType
+{
+	final TimePoint referenceTimepoint;
+	final HashSet< ViewId > fixedTiles;
+	
+	public ReferenceTimepointRegistration(
+			final SpimData2 spimData,
+			final ArrayList< Angle > anglesToProcess,
+			final ArrayList< ChannelProcess > channelsToProcess,
+			final ArrayList< Illumination > illumsToProcess,
+			final TimePoint referenceTimepoint,
+			final boolean save )
+	{ 
+		super( save );
+		
+		this.referenceTimepoint = referenceTimepoint;
+		this.fixedTiles = assembleFixedTiles( spimData, anglesToProcess, channelsToProcess, illumsToProcess, referenceTimepoint );
+	}
+	
+	/**
+	 * All tiles of the reference timepoint are fixed, nothing else
+	 * 
+	 * @param spimData
+	 * @param anglesToProcess
+	 * @param channelsToProcess
+	 * @param illumsToProcess
+	 * @param referenceTimepoint
+	 * @return
+	 */
+	protected HashSet< ViewId > assembleFixedTiles(
+			final SpimData2 spimData,
+			final ArrayList< Angle > anglesToProcess,
+			final ArrayList< ChannelProcess > channelsToProcess,
+			final ArrayList< Illumination > illumsToProcess,
+			final TimePoint referenceTimepoint )
+	{
+		final HashSet< ViewId > fixedTiles = new HashSet< ViewId >();
+		
+		for ( final Angle a : anglesToProcess )
+			for ( final Illumination i : illumsToProcess )
+				for ( final ChannelProcess c : channelsToProcess )
+				{
+					// bureaucracy
+					final ViewId viewId = SpimData2.getViewId( spimData.getSequenceDescription(), referenceTimepoint, c.getChannel(), a, i );
+					
+					if ( viewId == null )
+					{
+						throw new RuntimeException( "An error occured. Could not find the corresponding ViewSetup for reference timepoint: " + 
+								referenceTimepoint.getId() + " angle: " + 
+								a.getId() + " channel: " + c.getChannel().getId() + " illum: " + i.getId() );
+					}
+
+					final ViewDescription< TimePoint, ViewSetup > viewDescription = spimData.getSequenceDescription().getViewDescription( 
+							viewId.getTimePointId(), viewId.getViewSetupId() );
+	
+					if ( !viewDescription.isPresent() )
+						continue;
+					
+					fixedTiles.add( viewId );
+				}
+
+		return fixedTiles;
+	}
+
+	@Override
+	public boolean isFixedTile( final ViewId viewId, final GlobalOptimizationSubset set )
+	{
+		return fixedTiles.contains( viewId );
+	}
+
+	public TimePoint getReferenceTimepoint() { return referenceTimepoint; }
+
+	@Override
+	public List< GlobalOptimizationSubset > getAllViewPairs(
+			final SpimData2 spimData,
+			final ArrayList< Angle > anglesToProcess,
+			final ArrayList< ChannelProcess > channelsToProcess,
+			final ArrayList< Illumination > illumsToProcess,
+			final ArrayList< TimePoint > timepointsToProcess,
+			final int inputTransform,
+			final double minResolution )
+	{
+		final ArrayList< GlobalOptimizationSubset > list = new ArrayList< GlobalOptimizationSubset >();
+		
+		for ( final TimePoint timepoint : timepointsToProcess )
+		{
+			if ( timepoint == referenceTimepoint )
+				continue;
+			
+			final HashMap< ViewId, ChannelInterestPointList > pointLists = this.getInterestPoints(
+					spimData,
+					anglesToProcess,
+					channelsToProcess,
+					illumsToProcess,
+					timepoint,
+					inputTransform,
+					minResolution );
+			
+			final ArrayList< ViewId > views = new ArrayList< ViewId >();
+			views.addAll( pointLists.keySet() );
+			Collections.sort( views );
+			
+			final ArrayList< ChannelInterestPointListPair > viewPairs = new ArrayList< ChannelInterestPointListPair >();
+			
+			// all correspondences between the reference timepoint and the current timepoint
+			for ( int a = 0; a < views.size(); ++a )
+			{
+				final ViewId viewIdA = views.get( a );
+				final ChannelInterestPointList listA = pointLists.get( viewIdA );
+				
+				for ( final ViewId viewIdB : fixedTiles )
+				{
+					final ChannelInterestPointList listB = pointLists.get( viewIdB );
+					viewPairs.add( new ChannelInterestPointListPair( viewIdA, viewIdB, listA, listB ) );					
+				}
+			}
+			
+			// the views of the timepoint that is processed
+			for ( int a = 0; a < views.size() - 1; ++a )
+				for ( int b = a + 1; b < views.size(); ++b )
+				{
+					final ViewId viewIdA = views.get( a );
+					final ViewId viewIdB = views.get( b );
+					
+					final ChannelInterestPointList listA = pointLists.get( viewIdA );
+					final ChannelInterestPointList listB = pointLists.get( viewIdB );
+					
+					viewPairs.add( new ChannelInterestPointListPair( viewIdA, viewIdB, listA, listB ) );
+				}
+			
+			list.add( new GlobalOptimizationSubset( viewPairs, "reference timepoint ( " + referenceTimepoint.getName() + ", id=" + referenceTimepoint.getId() + 
+					") registration: " + timepoint.getName() + "(id=" + timepoint.getId() + ")" ) );
+		}
+		
+		return list;
+	}
+}
