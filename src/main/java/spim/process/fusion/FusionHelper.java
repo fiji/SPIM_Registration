1858a133fffc7b67bd2f27716e08f2885ebb59cf
Stephan Preibisch <stephan.preibisch@gmx.de>
2014-04-17 12:50:42 +0800

    implemented content-based fusion

--- src/main/java/spim/process/fusion/FusionHelper.java
+++ src/main/java/spim/process/fusion/FusionHelper.java
@@ -1,7 +1,12 @@
 package spim.process.fusion;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import mpicbg.spim.data.sequence.Angle;
 import mpicbg.spim.data.sequence.Channel;
@@ -10,6 +15,13 @@ import mpicbg.spim.data.sequence.TimePoint;
 import mpicbg.spim.data.sequence.ViewDescription;
 import mpicbg.spim.data.sequence.ViewId;
 import mpicbg.spim.data.sequence.ViewSetup;
+import mpicbg.spim.io.IOFunctions;
+import net.imglib2.Cursor;
+import net.imglib2.IterableInterval;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.view.Views;
 import spim.fiji.spimdata.SpimData2;
 
 public class FusionHelper 
@@ -56,6 +68,141 @@ public class FusionHelper
 
 		return inputData;
 	}
+	
+	public static < T extends RealType< T > > float[] minMax( final RandomAccessibleInterval< T > img )
+	{
+		final IterableInterval< T > iterable = Views.iterable( img );
+		
+		// split up into many parts for multithreading
+		final Vector< ImagePortion > portions = FusionHelper.divideIntoPortions( iterable.size(), Runtime.getRuntime().availableProcessors() * 2 );
+
+		// set up executor service
+		final ExecutorService taskExecutor = Executors.newFixedThreadPool( Runtime.getRuntime().availableProcessors() );
+		final ArrayList< Callable< float[] > > tasks = new ArrayList< Callable< float[] > >();
+
+		for ( final ImagePortion portion : portions )
+		{
+			tasks.add( new Callable< float[] >() 
+					{
+						@Override
+						public float[] call() throws Exception
+						{
+							float min = Float.MAX_VALUE;
+							float max = -Float.MAX_VALUE;
+							
+							final Cursor< T > c = iterable.cursor();
+							c.jumpFwd( portion.getStartPosition() );
+							
+							for ( long j = 0; j < portion.getLoopSize(); ++j )
+							{
+								final float v = c.next().getRealFloat();
+								
+								min = Math.min( min, v );
+								max = Math.max( max, v );
+							}
+							
+							// min & max of this portion
+							return new float[]{ min, max };
+						}
+					});
+		}
+		
+		// run threads and combine results
+		float min = Float.MAX_VALUE;
+		float max = -Float.MAX_VALUE;
+		
+		try
+		{
+			// invokeAll() returns when all tasks are complete
+			final List< Future< float[] > > futures = taskExecutor.invokeAll( tasks );
+			
+			for ( final Future< float[] > future : futures )
+			{
+				final float[] minmax = future.get();
+				min = Math.min( min, minmax[ 0 ] );
+				max = Math.max( max, minmax[ 1 ] );
+			}
+		}
+		catch ( final Exception e )
+		{
+			IOFunctions.println( "Failed to compute min/max: " + e );
+			e.printStackTrace();
+			return null;
+		}
+
+		taskExecutor.shutdown();
+		
+		return new float[]{ min, max };
+	}
+	
+	/**
+	 * Normalizes the image to the range [0...1]
+	 * 
+	 * @param image - the image to normalize
+	 * @return - a new float[] array with array[ 0 ] == min and array[ 1 ] == max
+	 */
+	public static boolean normalizeImage( final RandomAccessibleInterval< FloatType > img )
+	{
+		final float minmax[] = minMax( img );
+		final float min = minmax[ 0 ];
+		final float max = minmax[ 1 ];
+		final float diff = max - min;
+
+		if ( Float.isNaN( diff ) || Float.isInfinite(diff) || diff == 0 )
+		{
+			IOFunctions.println( "Cannot normalize image, min=" + min + "  + max=" + max );
+			return false;
+		}
+
+		final IterableInterval< FloatType > iterable = Views.iterable( img );
+		
+		// split up into many parts for multithreading
+		final Vector< ImagePortion > portions = FusionHelper.divideIntoPortions( iterable.size(), Runtime.getRuntime().availableProcessors() * 2 );
+
+		// set up executor service
+		final ExecutorService taskExecutor = Executors.newFixedThreadPool( Runtime.getRuntime().availableProcessors() );
+		final ArrayList< Callable< String > > tasks = new ArrayList< Callable< String > >();
+
+		for ( final ImagePortion portion : portions )
+		{
+			tasks.add( new Callable< String >() 
+					{
+						@Override
+						public String call() throws Exception
+						{
+							final Cursor< FloatType > c = iterable.cursor();
+							c.jumpFwd( portion.getStartPosition() );
+							
+							for ( long j = 0; j < portion.getLoopSize(); ++j )
+							{
+								final FloatType t = c.next();
+								
+		            			final float norm = ( t.get() - min ) / diff;
+		            			t.set( norm );
+							}
+							
+							return "";
+						}
+					});
+		}
+		
+		try
+		{
+			// invokeAll() returns when all tasks are complete
+			taskExecutor.invokeAll( tasks );
+		}
+		catch ( final InterruptedException e )
+		{
+			IOFunctions.println( "Failed to compute min/max: " + e );
+			e.printStackTrace();
+			return false;
+		}
+
+		taskExecutor.shutdown();
+		
+		return true;
+	}
+
 
 	public static final Vector<ImagePortion> divideIntoPortions( final long imageSize, final int numPortions )
 	{
---
+++ src/main/java/spim/process/fusion/weights/ContentBased.java
@@ -0,0 +1,173 @@
+package spim.process.fusion.weights;
+
+import ij.ImageJ;
+import ij.ImagePlus;
+import net.imglib2.Cursor;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.RealInterval;
+import net.imglib2.RealRandomAccess;
+import net.imglib2.RealRandomAccessible;
+import net.imglib2.algorithm.fft2.FFTConvolution;
+import net.imglib2.converter.RealFloatConverter;
+import net.imglib2.converter.read.ConvertedRandomAccessibleInterval;
+import net.imglib2.exception.IncompatibleTypeException;
+import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.interpolation.randomaccess.NLinearInterpolatorFactory;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.complex.ComplexFloatType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.Util;
+import net.imglib2.view.Views;
+import spim.process.fusion.FusionHelper;
+
+/**
+ * Computes the content-based fusion on a given image
+ * 
+ * @author Stephan Preibisch (stephan.preibisch@gmx.de)
+ *
+ * @param <T>
+ */
+public class ContentBased< T extends RealType< T > > implements RealRandomAccessible< FloatType >
+{
+	/**
+	 * The Img containing the approxmimated content-based weights
+	 */
+	final Img< FloatType > contentBasedImg;
+	final int n;
+	
+	public ContentBased(
+			final RandomAccessibleInterval< T > input,
+			final ImgFactory< ComplexFloatType > imgFactory,
+			final double[] sigma1,
+			final double[] sigma2 )
+	{
+		this.n = input.numDimensions();
+		
+		this.contentBasedImg = approximateEntropy(
+				new ConvertedRandomAccessibleInterval< T, FloatType >( input, new RealFloatConverter< T >(),  new FloatType() ),
+				imgFactory,
+				sigma1,
+				sigma2 );		
+	}
+	
+	public Img< FloatType > getContentBasedImg() { return contentBasedImg; }
+	
+	protected Img< FloatType > approximateEntropy(
+			final RandomAccessibleInterval< FloatType > input,
+			final ImgFactory< ComplexFloatType > imgFactory,
+			final double[] sigma1,
+			final double[] sigma2 )
+
+	{
+		// the result
+		ImgFactory<FloatType> f;
+		try { f = imgFactory.imgFactory( new FloatType() ); } catch (IncompatibleTypeException e) { f = new ArrayImgFactory< FloatType >(); }
+		
+		final Img< FloatType > conv = f.create( input, new FloatType() );
+		
+		// compute I*sigma1
+		FFTConvolution< FloatType > fftConv = new FFTConvolution<FloatType>( input, createGaussianKernel( sigma1 ), conv, imgFactory );
+		fftConv.run();
+		
+		// compute ( I - I*sigma1 )^2
+		final Cursor< FloatType > c = conv.cursor();
+		final RandomAccess< FloatType > r = input.randomAccess();
+		
+		while ( c.hasNext() )
+		{
+			c.fwd();
+			r.setPosition( c );
+			
+			final float diff = c.get().get() - r.get().get();
+			c.get().set( diff * diff );
+		}
+
+		ImageJFunctions.show( conv );
+		
+		// compute ( ( I - I*sigma1 )^2 ) * sigma2
+		fftConv = new FFTConvolution<FloatType>( conv, createGaussianKernel( sigma2 ), imgFactory );
+		fftConv.run();
+
+		// normalize to [0...1]
+		FusionHelper.normalizeImage( conv );
+
+		return conv;
+	}
+	
+	@Override
+	public int numDimensions() { return contentBasedImg.numDimensions(); }
+
+	@Override
+	public RealRandomAccess<FloatType> realRandomAccess()
+	{ 
+		return Views.interpolate(
+			Views.extendZero( this.contentBasedImg ),
+			new NLinearInterpolatorFactory< FloatType >()
+			).realRandomAccess();
+	}
+
+	@Override
+	public RealRandomAccess<FloatType> realRandomAccess( final RealInterval interval )
+	{
+		return Views.interpolate(
+				Views.extendZero( this.contentBasedImg ),
+				new NLinearInterpolatorFactory< FloatType >()
+				).realRandomAccess( interval );
+	}
+
+	final private static Img< FloatType > createGaussianKernel( final double[] sigmas )
+	{
+		final int numDimensions = sigmas.length;
+
+		final long[] imageSize = new long[ numDimensions ];
+		final double[][] kernel = new double[ numDimensions ][];
+
+		for ( int d = 0; d < numDimensions; ++d )
+		{
+			kernel[ d ] = Util.createGaussianKernel1DDouble( sigmas[ d ], true );
+			imageSize[ d ] = kernel[ d ].length;
+		}
+
+		final Img< FloatType > kernelImg = ArrayImgs.floats( imageSize );
+
+		final Cursor< FloatType > cursor = kernelImg.localizingCursor();
+		final int[] position = new int[ numDimensions ];
+
+		while ( cursor.hasNext() )
+		{
+			cursor.fwd();
+			cursor.localize( position );
+
+			double value = 1;
+
+			for ( int d = 0; d < numDimensions; ++d )
+				value *= kernel[ d ][ position[ d ] ];
+
+			cursor.get().set( ( float ) value );
+		}
+
+		return kernelImg;
+	}
+	
+	public static void main( String[] args ) throws IncompatibleTypeException
+	{
+		new ImageJ();
+		
+		ImagePlus imp = new ImagePlus( "/Users/preibischs/workspace/TestLucyRichardson/src/resources/dros-1.tif" );
+		
+		Img< FloatType > img = ImageJFunctions.wrap( imp );
+
+		final double[] sigma1 = new double[]{ 20, 20 };
+		final double[] sigma2 = new double[]{ 30, 30 };
+		
+		ContentBased< FloatType > cb = new ContentBased<FloatType>( img, img.factory().imgFactory( new ComplexFloatType() ), sigma1, sigma2 );
+		
+		ImageJFunctions.show( cb.getContentBasedImg() );
+	}
+
+}
