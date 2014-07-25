5ff88586e5c9831959b2f1606576a8683e88b9cc
Stephan Preibisch <stephan.preibisch@gmx.de>
2014-07-25 16:22:17 -0400

    added new methods

--- src/main/java/spim/process/cuda/CUDASeparableConvolution.java
+++ src/main/java/spim/process/cuda/CUDASeparableConvolution.java
@@ -2,8 +2,25 @@ package spim.process.cuda;
 
 public interface CUDASeparableConvolution extends CUDAStandardFunctions
 {
-	// extern "C" void convolve_31( float *image, float *kernelX, float *kernelY, float *kernelZ, int imageW, int imageH, int imageD, bool convolveX, bool convolveY, bool convolveZ, int devCUDA );
+	// extern "C" bool convolve_31( float *image, float *kernelX, float *kernelY, float *kernelZ, int imageW, int imageH, int imageD, bool convolveX, bool convolveY, bool convolveZ, int devCUDA );
+
+	// extern "C" int multipleOfX_31();
+	// extern "C" int multipleOfY_31();
+	// extern "C" int multipleOfZ_31();
 
 	// In-place convolution with a maximal kernel diameter of 31
-	public void convolve_31( float[] image, float[] kernelX, float[] kernelY, float[] kernelZ, int imageW, int imageH, int imageD, boolean convolveX, boolean convolveY, boolean convolveZ, int devCUDA );
+	public boolean convolve_31( float[] image, float[] kernelX, float[] kernelY, float[] kernelZ, int imageW, int imageH, int imageD, boolean convolveX, boolean convolveY, boolean convolveZ, int devCUDA );
+
+	/**
+	 * @return - multiple of which number the dimension has to be to not fail
+	 */
+	public int multipleOfX_31();
+	/**
+	 * @return - multiple of which number the dimension has to be to not fail
+	 */
+	public int multipleOfY_31();
+	/**
+	 * @return - multiple of which number the dimension has to be to not fail
+	 */
+	public int multipleOfZ_31();
 }
