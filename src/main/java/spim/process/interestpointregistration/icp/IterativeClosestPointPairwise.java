9c13fe1bbbc790bf59b50746bb8b03080b99b68f
Stephan Preibisch <stephan.preibisch@gmx.de>
2014-09-22 15:49:59 -0400

    Fixed a longstanding bug in the ICP implementation. Use world instead of local coordinates, otherwise it always ends up being just one iteration

--- src/main/java/spim/process/interestpointregistration/icp/IterativeClosestPointPairwise.java
+++ src/main/java/spim/process/interestpointregistration/icp/IterativeClosestPointPairwise.java
@@ -2,13 +2,17 @@ package spim.process.interestpointregistration.icp;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 import mpicbg.icp.ICP;
+import mpicbg.imglib.util.Util;
 import mpicbg.models.IllDefinedDataPointsException;
 import mpicbg.models.Model;
 import mpicbg.models.NotEnoughDataPointsException;
+import mpicbg.models.Point;
 import mpicbg.models.PointMatch;
+import mpicbg.models.TranslationModel3D;
 import mpicbg.pointdescriptor.exception.NoSuitablePointsException;
 import mpicbg.spim.io.IOFunctions;
 import mpicbg.spim.mpicbg.PointMatchGeneric;
@@ -43,6 +47,13 @@ public class IterativeClosestPointPairwise implements Callable< ChannelInterestP
 
 		for ( final InterestPoint i : pair.getListB() )
 			listB.add( new Detection( i.getId(), i.getL() ) );
+
+		// use the world and not the local coordinates
+		for ( final Detection d : listA )
+			d.setUseW( true );
+
+		for ( final Detection d : listB )
+			d.setUseW( true );
 
 		final ICP< Detection > icp = new ICP<Detection>( listA, listB, (float)ip.getMaxDistance() );
 		
@@ -86,17 +97,17 @@ public class IterativeClosestPointPairwise implements Callable< ChannelInterestP
 			System.out.println( i + ": " + icp.getNumPointMatches() + " matches, avg error [px] " + icp.getAverageError() + ", max error [px] " + icp.getMaximalError() );
 		}
 		while ( !converged && ++i < ip.getMaxNumIterations() );
+
+		final ArrayList< PointMatchGeneric< Detection > > inliers = new ArrayList< PointMatchGeneric< Detection > >();
 		
-    	final ArrayList< PointMatchGeneric< Detection > > inliers = new ArrayList< PointMatchGeneric< Detection > >();
-    	
-    	for ( final PointMatch pm : icp.getPointMatches() )
-    		inliers.add( new PointMatchGeneric<Detection>( (Detection)pm.getP1(), (Detection)pm.getP2() ) );
-    	
+		for ( final PointMatch pm : icp.getPointMatches() )
+			inliers.add( new PointMatchGeneric<Detection>( (Detection)pm.getP1(), (Detection)pm.getP2() ) );
+
 		pair.setCandidates( inliers );
 		pair.setInliers( inliers );
 
-    	IOFunctions.println( "(" + new Date( System.currentTimeMillis() ) + "): " + comparison + ": Found " + icp.getNumPointMatches() + " matches, avg error [px] " + icp.getAverageError() + " after " + i + " iterations" );
-    	
+		IOFunctions.println( "(" + new Date( System.currentTimeMillis() ) + "): " + comparison + ": Found " + icp.getNumPointMatches() + " matches, avg error [px] " + icp.getAverageError() + " after " + i + " iterations" );
+
 		return pair;
 	}
 
@@ -110,4 +121,86 @@ public class IterativeClosestPointPairwise implements Callable< ChannelInterestP
 		pair.setCandidates( new ArrayList<PointMatchGeneric<Detection>>() );
 		pair.setInliers( new ArrayList<PointMatchGeneric<Detection>>() );
 	}
+
+	public static void main( final String[] args ) throws Exception
+	{
+		// test ICP
+		final ArrayList< Detection > listA = new ArrayList< Detection >();
+		final ArrayList< Detection > listB = new ArrayList< Detection >();
+		
+		listA.add( new Detection( 0, new float[]{ 10, 10, 0 } ) );
+		listB.add( new Detection( 0, new float[]{ 11, 13, 0 } ) ); // d = 3.16
+
+		final Random rnd = new Random( 43534 );
+		final float maxError = 4;
+
+		for ( int i = 0; i < 5; ++i )
+		{
+			final float x = rnd.nextFloat() * 10000 + 150;
+			final float y = rnd.nextFloat() * 10000 + 150;
+			
+			listA.add( new Detection( i, new float[]{ x, y, 0 } ) );
+			listB.add( new Detection( i, new float[]{ x + 2, y + 4, 0 } ) ); // d = 4.472, will be less than 4 once the first one matched
+		}
+
+		// use the world and not the local coordinates
+		for ( int i = 0; i < listA.size(); ++ i )
+		{
+			listA.get( i ).setUseW( true );
+			listB.get( i ).setUseW( true );
+			IOFunctions.println( Util.printCoordinates( listA.get( i ).getL() ) + " >>> " + Util.printCoordinates( listB.get( i ).getL() ) + ", d=" + Point.distance( listA.get( i ), listB.get( i ) ) );
+		}
+
+		final ICP< Detection > icp = new ICP< Detection >( listA, listB, maxError );
+		
+		// identity transform
+		TranslationModel3D model = new TranslationModel3D();
+
+		int i = 0;
+		double lastAvgError = 0;
+		int lastNumCorresponding = 0;
+
+		boolean converged = false;
+
+		do
+		{
+			System.out.println( "\n" + i );
+			System.out.println( "lastModel: " + model.toString() );
+
+			try
+			{
+				icp.runICPIteration( model, model );
+			}
+			catch ( NotEnoughDataPointsException e )
+			{
+				throw new NotEnoughDataPointsException( e );
+			}
+			catch ( IllDefinedDataPointsException e )
+			{
+				throw new IllDefinedDataPointsException( e );
+			}
+			catch ( NoSuitablePointsException e )
+			{
+				throw new NoSuitablePointsException( e.toString() );
+			}
+
+			System.out.println( "newModel: " + model.toString() );
+
+			System.out.println( "lastError: " + lastAvgError + ", lastNumCorresponding: " + lastNumCorresponding );
+			System.out.println( "thisError: " + icp.getAverageError() + ", thisNumCorresponding: " + icp.getAverageError() );
+
+			if ( lastNumCorresponding == icp.getNumPointMatches() && lastAvgError == icp.getAverageError() )
+				converged = true;
+
+			lastNumCorresponding = icp.getNumPointMatches();
+			lastAvgError = icp.getAverageError();
+			
+			System.out.println( i + ": " + icp.getNumPointMatches() + " matches, avg error [px] " + icp.getAverageError() + ", max error [px] " + icp.getMaximalError() );
+		}
+		while ( !converged && ++i < 100 );
+		
+
+		IOFunctions.println( "(" + new Date( System.currentTimeMillis() ) + "): Found " + icp.getNumPointMatches() + " matches, avg error [px] " + icp.getAverageError() + " after " + i + " iterations" );
+	}
+
 }
