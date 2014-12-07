83f67eb283411a3793b3dc38ff62f2bef2eb3e62
Stephan Preibisch <stephan.preibisch@gmx.de>
2014-12-07 18:18:33 +0100

    implemented optimal bounding box orientation

--- src/main/java/spim/process/fusion/boundingbox/AutomaticReorientation.java
+++ src/main/java/spim/process/fusion/boundingbox/AutomaticReorientation.java
@@ -7,12 +7,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import net.imglib2.realtransform.AffineTransform3D;
+import javax.media.j3d.Transform3D;
+import javax.vecmath.AxisAngle4f;
+import javax.vecmath.Matrix4f;
+import javax.vecmath.Point3f;
+import javax.vecmath.Vector3f;
+
 import mpicbg.imglib.util.Util;
-import mpicbg.models.Point;
 import mpicbg.spim.data.registration.ViewRegistration;
 import mpicbg.spim.data.registration.ViewRegistrations;
 import mpicbg.spim.data.registration.ViewTransform;
+import mpicbg.spim.data.registration.ViewTransformAffine;
 import mpicbg.spim.data.sequence.Angle;
 import mpicbg.spim.data.sequence.Channel;
 import mpicbg.spim.data.sequence.Illumination;
@@ -20,6 +25,7 @@ import mpicbg.spim.data.sequence.TimePoint;
 import mpicbg.spim.data.sequence.ViewDescription;
 import mpicbg.spim.data.sequence.ViewId;
 import mpicbg.spim.io.IOFunctions;
+import net.imglib2.realtransform.AffineTransform3D;
 import spim.fiji.ImgLib2Temp.Pair;
 import spim.fiji.ImgLib2Temp.ValuePair;
 import spim.fiji.plugin.Interest_Point_Registration;
@@ -170,58 +176,278 @@ public class AutomaticReorientation extends ManualBoundingBox
 		for ( final ChannelProcess c : channelsToUse )
 			IOFunctions.println( "using from channel: " + c.getChannel().getId()  + " label: '" + c.getLabel() + "', " + (detections == 0 ? "all detections." : "only corresponding detections.") );
 
-		
+		// to be filled
+		final float[] minF, maxF;
+
 		if ( reorientate == 3 )
 		{
 			final Pair< float[], float[] > minmax = determineSizeSimple( channelsToUse, detections );
 
-			final float[] minF = minmax.getA();
-			final float[] maxF = minmax.getB();
-			final int[] min = new int[ minF.length ];
-			final int[] max = new int[ maxF.length ];
-
-			float addX = (maxF[ 0 ] - minF[ 0 ]) * (float)( percentX/100.0 ) / 2;
-			float addY = (maxF[ 1 ] - minF[ 1 ]) * (float)( percentY/100.0 ) / 2;
-			float addZ = (maxF[ 2 ] - minF[ 2 ]) * (float)( percentZ/100.0 ) / 2;
-
-			min[ 0 ] = Math.round( minF[ 0 ] - addX );
-			min[ 1 ] = Math.round( minF[ 1 ] - addY );
-			min[ 2 ] = Math.round( minF[ 2 ] - addZ );
-
-			max[ 0 ] = Math.round( maxF[ 0 ] + addX );
-			max[ 1 ] = Math.round( maxF[ 1 ] + addY );
-			max[ 2 ] = Math.round( maxF[ 2 ] + addZ );
-
-			IOFunctions.println( "Min (with addition): " + Util.printCoordinates( min ) );
-			IOFunctions.println( "Max (with addition): " + Util.printCoordinates( max ) );
-
-			BoundingBox.defaultMin = min;
-			BoundingBox.defaultMax = max;
+			minF = minmax.getA();
+			maxF = minmax.getB();
 		}
 		else
 		{
-			determineOptimalBoundingBox( channelsToUse, detections );
-			new RuntimeException( "Not implemented yet." );
+			final Pair< AffineTransform3D, float[] > pair = determineOptimalBoundingBox( channelsToUse, detections );
+
+			//
+			// set the registration for all or some of the views
+			//
+			IOFunctions.println( "Final transformation model: " + pair.getA() );
+
+			final List< TimePoint > tps;
+			final List< Channel > chns;
+			final List< Illumination > ills;
+			final List< Angle > angls;
+
+			if ( reorientate == 0 ) // apply to all views
+			{
+				tps = spimData.getSequenceDescription().getTimePoints().getTimePointsOrdered();
+				chns = spimData.getSequenceDescription().getAllChannelsOrdered();
+				ills = spimData.getSequenceDescription().getAllIlluminationsOrdered();
+				angls = spimData.getSequenceDescription().getAllAnglesOrdered();
+
+				IOFunctions.println( "Will be applied only to all views and saved to the XML." );
+			}
+			else // apply only to fused views
+			{
+				tps = timepointsToProcess;
+				chns = channelsToProcess;
+				ills = illumsToProcess;
+				angls = anglesToProcess;
+
+				if ( reorientate == 1 )
+					IOFunctions.println( "Will be applied only to fused views and saved to the XML." );
+				else
+					IOFunctions.println( "Will be temporarily applied only to fused views (NOT saved to XML)." );
+			}
+
+			for ( final TimePoint t : tps )
+				for ( final Channel c : chns )
+					for ( final Illumination i : ills )
+						for ( final Angle a : angls )
+						{
+							final ViewId viewId = SpimData2.getViewId( spimData.getSequenceDescription(), t, c, a, i );
+
+							// this happens only if a viewsetup is not present in any timepoint
+							// (e.g. after appending fusion to a dataset)
+							if ( viewId == null )
+								continue;
+
+							final ViewDescription vd = spimData.getSequenceDescription().getViewDescription( viewId );
+							
+							if ( !vd.isPresent() )
+								continue;
+
+							// get the registration
+							final ViewRegistration r = spimData.getViewRegistrations().getViewRegistration( viewId );
+							r.preconcatenateTransform( new ViewTransformAffine( reorientationDescription, pair.getA() ) );
+							r.updateModel();
+						}
+
+			minF = new float[]{ pair.getB()[ 0 ], pair.getB()[ 1 ], pair.getB()[ 2 ] };
+			maxF = new float[]{ pair.getB()[ 3 ], pair.getB()[ 4 ], pair.getB()[ 5 ] };
 		}
+
+		IOFunctions.println( "Min (without addition): " + Util.printCoordinates( minF ) );
+		IOFunctions.println( "Max (without addition): " + Util.printCoordinates( maxF ) );
+
+		final int[] min = new int[ 3 ];
+		final int[] max = new int[ 3 ];
+
+		float addX = (maxF[ 0 ] - minF[ 0 ]) * (float)( percentX/100.0 ) / 2;
+		float addY = (maxF[ 1 ] - minF[ 1 ]) * (float)( percentY/100.0 ) / 2;
+		float addZ = (maxF[ 2 ] - minF[ 2 ]) * (float)( percentZ/100.0 ) / 2;
+
+		min[ 0 ] = Math.round( minF[ 0 ] - addX );
+		min[ 1 ] = Math.round( minF[ 1 ] - addY );
+		min[ 2 ] = Math.round( minF[ 2 ] - addZ );
+
+		max[ 0 ] = Math.round( maxF[ 0 ] + addX );
+		max[ 1 ] = Math.round( maxF[ 1 ] + addY );
+		max[ 2 ] = Math.round( maxF[ 2 ] + addZ );
+
+		IOFunctions.println( "Min (with addition): " + Util.printCoordinates( min ) );
+		IOFunctions.println( "Max (with addition): " + Util.printCoordinates( max ) );
+
+		BoundingBox.defaultMin = min;
+		BoundingBox.defaultMax = max;
 
 		return super.queryParameters( fusion, imgExport );
 	}
 
-	protected void determineOptimalBoundingBox( final ArrayList< ChannelProcess > channelsToUse, final int detections )
+	/**
+	 * 
+	 * @param channelsToUse
+	 * @param detections
+	 * @return - the transformation and minX, minY, minZ, maxX, maxY, maxZ as Pair
+	 */
+	protected Pair< AffineTransform3D, float[] > determineOptimalBoundingBox( final ArrayList< ChannelProcess > channelsToUse, final int detections )
 	{
 		final List< float[] > points = getAllDetectionsInGlobalCoordinates( channelsToUse, detections );
 
 		// identify most distant points
-		double maxDist = -1;
-		float[] p1, p2;
+		float[] p1 = points.get( 0 );
+		float[] p2 = points.get( 1 );
+		double maxDist = squareDistance( p1[ 0 ], p1[ 1 ], p1[ 2 ], p2[ 0 ], p2[ 1 ], p2[ 2 ] );
 
 		for ( int i = 0; i < points.size() - 1; ++i )
 			for ( int j = i + 1; j < points.size(); ++j )
 			{
-				//squareDistance( p1[ 0 ], p1[ 1 ], p1[ 2 ], p2[ 0 ], p2[ 1 ], p2[ 2 ] );
+				final float[] a = points.get( i );
+				final float[] b = points.get( j );
+
+				final float d = squareDistance( a[ 0 ], a[ 1 ], a[ 2 ], b[ 0 ], b[ 1 ], b[ 2 ] );
+
+				if ( d > maxDist )
+				{
+					maxDist = d;
+					if ( a[ 0 ] <= b[ 0 ] )
+					{
+						p1 = a;
+						p2 = b;
+					}
+					else
+					{
+						p1 = b;
+						p2 = a;
+					}
+				}
 			}
+
+		final Vector3f sv = new Vector3f( p2[ 0 ] - p1[ 0 ], p2[ 1 ] - p1[ 1 ], p2[ 2 ] - p1[ 2 ] );
+		sv.normalize();
+
+		IOFunctions.println(
+				"Maximum distance: " + Math.sqrt( maxDist ) + "px between points " + Util.printCoordinates( p1 ) +
+				" and " + Util.printCoordinates( p2 ) + ", vector=" + sv + ", volume=" +
+				testAxis( sv, points ).getA() / (1024*1024) + "MiPixels." );
+
+		final float[] vectorStep = new float[]{ 0.4f, 0.2f, 0.1f, 0.05f, 0.025f, 0.01f, 0.005f, 0.001f };
+
+		double minVolume = Double.MAX_VALUE;
+		float[] minBoundingBox = null;
+
+		for ( int stepIndex = 0; stepIndex < vectorStep.length; ++stepIndex )
+		{
+			final float step = vectorStep[ stepIndex ];
+
+			// the best search vector found so far on this scale
+			final Vector3f bestSV = new Vector3f( sv );
+
+			for ( int zi = -1; zi <= 1; ++zi )
+				for ( int yi = -1; yi <= 1; ++yi )
+					for ( int xi = -1; xi <= 1; ++xi )
+					{
+						// compute the test vector
+						final Vector3f v = new Vector3f(
+								sv.x + xi * step,
+								sv.y + yi * step,
+								sv.z + zi * step );
+
+						v.normalize();
+
+						final Pair< Double, float[] > volume = testAxis( v, points );
+
+						if ( volume.getA() < minVolume )
+						{
+							minVolume = volume.getA();
+							minBoundingBox = volume.getB();
+							bestSV.set( v );
+
+							IOFunctions.println( "Scale: " + step + " --- Min Volume: " + minVolume / (1024*1024) + "MiPixels, vector=" + bestSV );
+						}
+					}
+
+			// update the search vector to the best solution from this scale
+			sv.set( bestSV );
+		}
+
+		// final mapping v onto the x axis
+		final Vector3f xAxis = new Vector3f( 1, 0, 0 );
+		final Matrix4f m = new Matrix4f();
+		getRotation( sv, xAxis ).get( m );
+
+		final AffineTransform3D a = new AffineTransform3D();
+		a.set( m.m00, m.m01, m.m02, m.m03, m.m10, m.m11, m.m12, m.m13, m.m20, m.m21, m.m22, m.m23 );
+
+		return new ValuePair< AffineTransform3D, float[] >( a, minBoundingBox );
 	}
 
+	
+
+	/**
+	 * Test one major axis for the minimal bounding box volume required
+	 * 
+	 * @param v - normalized vector
+	 * @param points - all points
+	 * @return - the volume and minX, minY, minZ, maxX, maxY, maxZ as Pair
+	 */
+	protected Pair< Double, float[] > testAxis( final Vector3f v, final List< float[] > points )
+	{
+		// mapping v onto the x axis
+		final Vector3f xAxis = new Vector3f( 1, 0, 0 );
+		final Transform3D t = getRotation( v, xAxis );
+
+		final Point3f tmp = new Point3f();
+
+		float minX = Float.MAX_VALUE;
+		float minY = Float.MAX_VALUE;
+		float minZ = Float.MAX_VALUE;
+
+		float maxX = -Float.MAX_VALUE;
+		float maxY = -Float.MAX_VALUE;
+		float maxZ = -Float.MAX_VALUE;
+
+		for ( final float[] p : points )
+		{
+			// transform onto the x-axis
+			tmp.set( p[ 0 ], p[ 1 ], p[ 2 ] );
+			t.transform( tmp );
+
+			minX = Math.min( minX, tmp.x );
+			minY = Math.min( minY, tmp.y );
+			minZ = Math.min( minZ, tmp.z );
+
+			maxX = Math.max( maxX, tmp.x );
+			maxY = Math.max( maxY, tmp.y );
+			maxZ = Math.max( maxZ, tmp.z );
+		}
+
+		return new ValuePair< Double, float[] >(
+				(double)( maxX - minX ) * (double)( maxY - minY ) * (double)( maxZ - minZ ),
+				new float[]{ minX, minY, minZ, maxX, maxY, maxZ } );
+	}
+
+	/**
+	 * Computes a Transform3D that will rotate vector v0 into the direction of vector v1.
+	 * Note: vectors MUST be normalized for this to work!
+	 *
+	 * @param v0
+	 * @param v1
+	 * @return
+	 */
+	public static Transform3D getRotation( final Vector3f v0, final Vector3f v1 )
+	{
+		// the rotation axis is defined by the cross product
+		final Vector3f rotAxis = new Vector3f();
+		rotAxis.cross( v0, v1 );
+		rotAxis.normalize();
+
+		// if the cross product returns NaN, the vectors already point in the same direction,
+		// so we return the identity transform
+		if ( Float.isNaN( rotAxis.x ) )
+			return new Transform3D();
+
+		// the rotation angle is defined by the dot product (if normalized)
+		final float angle = v0.dot( v1 );
+
+		// Do an axis/angle 3d transformation
+		final Transform3D t = new Transform3D();
+		t.set( new AxisAngle4f( rotAxis, (float)Math.acos( angle ) ) );
+
+		return t;
+	}
 	final static public float squareDistance( final float p1x, final float p1y, final float p1z, final float p2x, final float p2y, final float p2z )
 	{
 		final double dx = p1x - p2x;
