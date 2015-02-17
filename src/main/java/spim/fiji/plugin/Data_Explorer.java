95a80d5bb59116aa0b231718925001819ac78a5b
Stephan Preibisch <stephan.preibisch@gmx.de>
2015-02-17 11:56:29 -0500

    DataExplorer can load a dataset or create a new one

--- src/main/java/spim/fiji/plugin/Data_Explorer.java
+++ src/main/java/spim/fiji/plugin/Data_Explorer.java
@@ -1,5 +1,11 @@
 package spim.fiji.plugin;
 
+import ij.plugin.PlugIn;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import spim.fiji.ImgLib2Temp.Pair;
 import spim.fiji.plugin.queryXML.GenericLoadParseQueryXML;
 import spim.fiji.plugin.queryXML.LoadParseQueryXML;
 import spim.fiji.spimdata.SpimData2;
@@ -7,22 +13,61 @@ import spim.fiji.spimdata.XmlIoSpimData2;
 import spim.fiji.spimdata.explorer.ViewSetupExplorer;
 import spim.fiji.spimdata.explorer.interestpoint.InterestPointExplorer;
 import spim.fiji.spimdata.explorer.registration.RegistrationExplorer;
-import ij.plugin.PlugIn;
 
 public class Data_Explorer implements PlugIn
 {
+	boolean newDataset = false;
+
 	@Override
 	public void run( String arg )
 	{
 		final LoadParseQueryXML result = new LoadParseQueryXML();
 
-		if ( !result.queryXML( "XML Explorer", "", false, false, false, false ) )
+		result.addButton( "Define a new dataset", new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				result.setReturnFalse( true );
+				result.getGenericDialog().dispose();
+				setDefineNewDataset();
+			}
+		});
+
+		if ( !result.queryXML( "XML Explorer", "", false, false, false, false ) && !newDataset )
 			return;
 
-		final ViewSetupExplorer< SpimData2, XmlIoSpimData2 > explorer = new ViewSetupExplorer<SpimData2, XmlIoSpimData2 >( result.getData(), result.getXMLFileName(), result.getIO() );
+		final SpimData2 data;
+		final String xml;
+		final XmlIoSpimData2 io;
 
-		new InterestPointExplorer< SpimData2, XmlIoSpimData2 >( result.getData(), result.getXMLFileName(), result.getIO(), explorer );
-		new RegistrationExplorer< SpimData2, XmlIoSpimData2 >( result.getData(), result.getXMLFileName(), result.getIO(), explorer );
+		if ( newDataset )
+		{
+			final Pair< SpimData2, String > dataset = new Define_Multi_View_Dataset().defineDataset( true );
+
+			if ( dataset == null )
+				return;
+
+			data = dataset.getA();
+			xml = dataset.getB();
+			io = new XmlIoSpimData2( "" );
+		}
+		else
+		{
+			data = result.getData();
+			xml = result.getXMLFileName();
+			io = result.getIO();
+		}
+
+		final ViewSetupExplorer< SpimData2, XmlIoSpimData2 > explorer = new ViewSetupExplorer<SpimData2, XmlIoSpimData2 >( data, xml, io );
+
+		new InterestPointExplorer< SpimData2, XmlIoSpimData2 >( data, xml, io, explorer );
+		new RegistrationExplorer< SpimData2, XmlIoSpimData2 >( data, xml, io, explorer );
+	}
+
+	protected void setDefineNewDataset()
+	{
+		this.newDataset = true;
 	}
 
 	public static void main( String[] args )
