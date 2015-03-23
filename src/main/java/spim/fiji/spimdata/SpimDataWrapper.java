ff1ab7505b153d083e4da7603bc54ce015dd55a2
Stephan Preibisch <stephan.preibisch@gmx.de>
2015-03-23 09:50:49 -0400

    spimdatawrapper works on abstractspimdata

--- src/main/java/spim/fiji/spimdata/SpimDataWrapper.java
+++ src/main/java/spim/fiji/spimdata/SpimDataWrapper.java
@@ -1,6 +1,6 @@
 package spim.fiji.spimdata;
 
-import mpicbg.spim.data.SpimData;
+import mpicbg.spim.data.generic.AbstractSpimData;
 import mpicbg.spim.data.generic.sequence.BasicImgLoader;
 import bdv.spimdata.SequenceDescriptionMinimal;
 import bdv.spimdata.SpimDataMinimal;
@@ -10,7 +10,7 @@ public class SpimDataWrapper extends SpimDataMinimal
 	/**
 	 * create copy of a {@link SpimDataMinimal} with replaced {@link BasicImgLoader}
 	 */
-	public SpimDataWrapper( final SpimData other )
+	public SpimDataWrapper( final AbstractSpimData< ? > other )
 	{
 		super(
 				other.getBasePath(),
