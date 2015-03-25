43be096ce01cc5cf002fc541b42dfcbcf91cb227
Stephan Preibisch <stephan.preibisch@gmx.de>
2015-03-25 13:06:30 -0400

    added an empty imgloader

---
+++ src/main/java/spim/fiji/spimdata/imgloaders/MicroManagerImgLoader.java
@@ -0,0 +1,49 @@
+package spim.fiji.spimdata.imgloaders;
+
+import java.io.File;
+
+import mpicbg.spim.data.generic.sequence.AbstractSequenceDescription;
+import mpicbg.spim.data.sequence.ViewId;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.ImgFactory;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.type.numeric.real.FloatType;
+
+public class MicroManagerImgLoader extends AbstractImgLoader
+{
+	final File mmFile;
+	final AbstractSequenceDescription<?, ?, ?> sequenceDescription;
+
+	public MicroManagerImgLoader(
+			final File mmFile,
+			final ImgFactory< ? extends NativeType< ? > > imgFactory,
+			final AbstractSequenceDescription<?, ?, ?> sequenceDescription )
+	{
+		super();
+		this.mmFile = mmFile;
+		this.sequenceDescription = sequenceDescription;
+
+		setImgFactory( imgFactory );
+	}
+
+	@Override
+	public RandomAccessibleInterval<FloatType> getFloatImage(ViewId view,
+			boolean normalize) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RandomAccessibleInterval<UnsignedShortType> getImage(ViewId view) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void loadMetaData(ViewId view) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
