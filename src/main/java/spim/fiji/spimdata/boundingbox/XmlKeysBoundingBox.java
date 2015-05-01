1f9cd2b1b0f2b1e691bee1ba115765fb3c194263
Stephan Preibisch <stephan.preibisch@gmx.de>
2015-05-01 16:46:22 -0400

    added the bounding box implementation

---
+++ src/main/java/spim/fiji/spimdata/boundingbox/XmlKeysBoundingBox.java
@@ -0,0 +1,11 @@
+package spim.fiji.spimdata.boundingbox;
+
+public class XmlKeysBoundingBox
+{
+	public static final String BOUNDINGBOXES_TAG = "BoundingBoxes";
+	public static final String BOUNDINGBOX_TAG = "BoundingBoxDefinition";
+
+	public static final String BOUNDINGBOX_TAG_NAME = "name";
+	public static final String BOUNDINGBOX_TAG_MIN = "min";
+	public static final String BOUNDINGBOX_TAG_MAX = "max";
+}
