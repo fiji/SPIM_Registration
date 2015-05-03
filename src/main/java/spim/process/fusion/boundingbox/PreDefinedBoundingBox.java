02b98dbf8ae8a36465b615a5708d5e9797fb2745
Stephan Preibisch <stephan.preibisch@gmx.de>
2015-05-03 17:39:53 -0400

    predefined bounding box only shows names and dimensions & offset as label so that macro recording is simpler

--- src/main/java/spim/process/fusion/boundingbox/PreDefinedBoundingBox.java
+++ src/main/java/spim/process/fusion/boundingbox/PreDefinedBoundingBox.java
@@ -1,12 +1,17 @@
 package spim.process.fusion.boundingbox;
 
+import ij.gui.DialogListener;
 import ij.gui.GenericDialog;
 
+import java.awt.AWTEvent;
+import java.awt.Choice;
+import java.awt.Label;
 import java.util.List;
 
 import mpicbg.spim.data.sequence.ViewId;
 import mpicbg.spim.io.IOFunctions;
 import spim.fiji.plugin.fusion.Fusion;
+import spim.fiji.plugin.util.GUIHelper;
 import spim.fiji.spimdata.SpimData2;
 import spim.fiji.spimdata.boundingbox.BoundingBox;
 import spim.process.fusion.export.ImgExport;
@@ -34,12 +39,22 @@ public class PreDefinedBoundingBox extends BoundingBoxGUI
 		final String[] boundingBoxes = new String[ spimData.getBoundingBoxes().getBoundingBoxes().size() ];
 
 		for ( int i = 0; i < boundingBoxes.length; ++i )
-			boundingBoxes[ i ] = getBoundingBoxDescription( spimData.getBoundingBoxes().getBoundingBoxes().get( i ) );
+			boundingBoxes[ i ] = spimData.getBoundingBoxes().getBoundingBoxes().get( i ).getTitle();
 
 		if ( defaultBoundingBox >= boundingBoxes.length )
 			defaultBoundingBox = 0;
 
 		gd.addChoice( "Select_Bounding_Box", boundingBoxes, boundingBoxes[ defaultBoundingBox ] );
+		final Choice choice = (Choice)gd.getChoices().lastElement();
+
+		gd.addMessage( "" );
+		gd.addMessage( "BoundingBox size: ???x???x??? pixels", GUIHelper.mediumstatusfont );
+		final Label l1 = (Label)gd.getMessage();
+
+		gd.addMessage( "BoundingBox offset: ???x???x??? pixels", GUIHelper.mediumstatusfont );
+		final Label l2 = (Label)gd.getMessage();
+
+		addListeners( gd, choice, l1, l2 );
 
 		gd.showDialog();
 
@@ -81,6 +96,40 @@ public class PreDefinedBoundingBox extends BoundingBoxGUI
 	public String getDescription()
 	{
 		return "Use pre-defined Bounding Box";
+	}
+
+	protected DialogListener addListeners(
+			final GenericDialog gd,
+			final Choice choice,
+			final Label label1,
+			final Label label2 )
+	{
+		DialogListener d = new DialogListener()
+		{
+			@Override
+			public boolean dialogItemChanged( final GenericDialog dialog, final AWTEvent e )
+			{
+				if ( e.getSource() == choice )
+					update( spimData, choice, label1, label2 );
+
+				return true;
+			}
+		};
+
+		gd.addDialogListener( d );
+
+		update( spimData, choice, label1, label2 );
+
+		return d;
+	}
+
+	protected final static void update( final SpimData2 spimData, final Choice choice, final Label label1, final Label label2 )
+	{
+		final int index = choice.getSelectedIndex();
+		final BoundingBox bb = spimData.getBoundingBoxes().getBoundingBoxes().get( index );
+
+		label1.setText( "BoundingBox size: " + bb.dimension( 0 ) + "x" + bb.dimension( 1 ) + "x" + bb.dimension( 0 ) + " pixels" );
+		label2.setText( "BoundingBox offset: " + bb.min( 0 ) + "x" + bb.min( 1 ) + "x" + bb.min( 2 ) + " pixels" );
 	}
 
 }
