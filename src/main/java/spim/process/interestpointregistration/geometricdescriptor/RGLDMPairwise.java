01c051eef648772f98da3e64ccf9fe33f4a27ea3
Stephan Preibisch <stephan.preibisch@gmx.de>
2015-07-02 13:07:18 +0200

    refactored pairwise registration methods, doesn't compile now clean separation between GUI, parameters, and processing

--- src/main/java/spim/process/interestpointregistration/geometricdescriptor/RGLDMPairwise.java
+++ src/main/java/spim/process/interestpointregistration/geometricdescriptor/RGLDMPairwise.java
@@ -4,51 +4,61 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.Callable;
 
-import mpicbg.spim.io.IOFunctions;
 import mpicbg.spim.mpicbg.PointMatchGeneric;
 import spim.fiji.ImgLib2Temp.Pair;
 import spim.fiji.spimdata.interestpoints.InterestPoint;
+import spim.fiji.spimdata.interestpoints.InterestPointList;
+import spim.headless.registration.PairwiseResult;
+import spim.headless.registration.RANSACParameters;
+import spim.headless.registration.geometricdescriptor.RGLDMParameters;
 import spim.process.interestpointregistration.Detection;
-import spim.process.interestpointregistration.PairwiseMatch;
 import spim.process.interestpointregistration.RANSAC;
-import spim.process.interestpointregistration.RANSACParameters;
-import spim.process.interestpointregistration.TransformationModel;
 
-public class RGLDMPairwise implements Callable< PairwiseMatch >
-{	
-	final PairwiseMatch pair;
-	final TransformationModel model;
+public class RGLDMPairwise implements Callable< PairwiseResult >
+{
+	final InterestPointList listA;
+	final InterestPointList listB;
+	final PairwiseResult result;
 	final RANSACParameters rp;
 	final RGLDMParameters dp;
-	final String comparison;
 
-	public RGLDMPairwise( final PairwiseMatch pair, final TransformationModel model, final String comparison, final RANSACParameters rp, final RGLDMParameters dp  )
+	public RGLDMPairwise(
+			final InterestPointList listA,
+			final InterestPointList listB,
+			final RANSACParameters rp,
+			final RGLDMParameters dp  )
 	{
-		this.pair = pair;
+		this.listA = listA;
+		this.listB = listB;
+		this.result = new PairwiseResult();
 		this.rp = rp;
 		this.dp = dp;
-		this.model = model;
-		this.comparison = comparison;
 	}
 	
 	@Override
-	public PairwiseMatch call() throws Exception
+	public PairwiseResult call() throws Exception
 	{
 		final ArrayList< Detection > listA = new ArrayList< Detection >();
 		final ArrayList< Detection > listB = new ArrayList< Detection >();
-		
-		for ( final InterestPoint i : pair.getListA() )
+
+		if ( this.listA.getInterestPoints() == null )
+			this.listA.loadInterestPoints();
+
+		if ( this.listB.getInterestPoints() == null )
+			this.listB.loadInterestPoints();
+
+		for ( final InterestPoint i : this.listA.getInterestPoints() )
 			listA.add( new Detection( i.getId(), i.getL() ) );
 
-		for ( final InterestPoint i : pair.getListB() )
+		for ( final InterestPoint i : this.listB.getInterestPoints() )
 			listB.add( new Detection( i.getId(), i.getL() ) );
 
 		if ( listA.size() < 4 || listB.size() < 4 )
 		{
-			IOFunctions.println( "(" + new Date( System.currentTimeMillis() ) + "): " + comparison + ": Not enough detections to match" );
-			pair.setCandidates( new ArrayList< PointMatchGeneric< Detection > >() );
-			pair.setInliers( new ArrayList< PointMatchGeneric< Detection > >(), Double.NaN );
-			return pair;
+			result.result = "(" + new Date( System.currentTimeMillis() ) + "): " /*+ comparison */ + ": Not enough detections to match";
+			result.setCandidates( new ArrayList< PointMatchGeneric< Detection > >() );
+			result.setInliers( new ArrayList< PointMatchGeneric< Detection > >(), Double.NaN );
+			return result;
 		}
 
 		final RGLDMMatcher matcher = new RGLDMMatcher();
@@ -60,18 +70,17 @@ public class RGLDMPairwise implements Callable< PairwiseMatch >
 				dp.getRatioOfDistance(),
 				dp.getDifferenceThreshold() );
 
-		pair.setCandidates( candidates );
+		result.setCandidates( candidates );
 
 		// compute ransac and remove inconsistent candidates
 		final ArrayList< PointMatchGeneric< Detection > > inliers = new ArrayList< PointMatchGeneric< Detection > >();
 	
-		final Pair< String, Double > result = RANSAC.computeRANSAC( candidates, inliers, this.model.getModel(), rp.getMaxEpsilon(), rp.getMinInlierRatio(), rp.getMinInlierFactor(), rp.getNumIterations() );
+		final Pair< String, Double > ransacResult = RANSAC.computeRANSAC( candidates, inliers, dp.getModel(), rp.getMaxEpsilon(), rp.getMinInlierRatio(), rp.getMinInlierFactor(), rp.getNumIterations() );
 	
-		pair.setInliers( inliers, result.getB() );
+		result.setInliers( inliers, ransacResult.getB() );
 	
-		IOFunctions.println( "(" + new Date( System.currentTimeMillis() ) + "): " + comparison + ": " + result.getA() );
+		result.result = "(" + new Date( System.currentTimeMillis() ) + "): " + /*comparison +*/ ": " + ransacResult.getA();
 		
-		return pair;
+		return result;
 	}
-
 }
--- src/main/java/spim/process/interestpointregistration/geometrichashing/GeometricHashingPairwise.java
+++ src/main/java/spim/process/interestpointregistration/geometrichashing/GeometricHashingPairwise.java
@@ -4,63 +4,63 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.Callable;
 
-import mpicbg.spim.io.IOFunctions;
 import mpicbg.spim.mpicbg.PointMatchGeneric;
 import spim.fiji.ImgLib2Temp.Pair;
 import spim.fiji.spimdata.interestpoints.InterestPoint;
+import spim.fiji.spimdata.interestpoints.InterestPointList;
+import spim.headless.registration.PairwiseResult;
+import spim.headless.registration.RANSACParameters;
+import spim.headless.registration.geometrichashing.GeometricHashingParameters;
 import spim.process.interestpointregistration.Detection;
-import spim.process.interestpointregistration.PairwiseMatch;
 import spim.process.interestpointregistration.RANSAC;
-import spim.process.interestpointregistration.RANSACParameters;
-import spim.process.interestpointregistration.TransformationModel;
 
-public class GeometricHashingPairwise implements Callable< PairwiseMatch >
+public class GeometricHashingPairwise implements Callable< PairwiseResult >
 {
-	final PairwiseMatch pair;
-	final TransformationModel model;
+	final InterestPointList listA;
+	final InterestPointList listB;
+	final PairwiseResult result;
 	final RANSACParameters rp;
 	final GeometricHashingParameters gp;
-	final String comparison;
 	
-	public GeometricHashingPairwise( final PairwiseMatch pair, final TransformationModel model, final String comparison, final RANSACParameters rp, final GeometricHashingParameters gp )
+	public GeometricHashingPairwise(
+			final InterestPointList listA,
+			final InterestPointList listB,
+			final RANSACParameters rp,
+			final GeometricHashingParameters gp )
 	{ 
-		this.pair = pair;
+		this.listA = listA;
+		this.listB = listB;
+		this.result = new PairwiseResult();
 		this.rp = rp;
 		this.gp = gp;
-		this.model = model;
-		this.comparison = comparison;
 	}
 
-	public GeometricHashingPairwise( final PairwiseMatch pair, final TransformationModel model, final String comparison, final RANSACParameters rp )
-	{
-		this( pair, model, comparison, rp, new GeometricHashingParameters() );
-	}
-
-	public GeometricHashingPairwise( final PairwiseMatch pair, final TransformationModel model, final String comparison )
-	{
-		this( pair, model, comparison, new RANSACParameters(), new GeometricHashingParameters() );
-	}
-	
 	@Override
-	public PairwiseMatch call() throws Exception 
+	public PairwiseResult call() throws Exception 
 	{
 		final GeometricHasher hasher = new GeometricHasher();
 		
 		final ArrayList< Detection > listA = new ArrayList< Detection >();
 		final ArrayList< Detection > listB = new ArrayList< Detection >();
-		
-		for ( final InterestPoint i : pair.getListA() )
+
+		if ( this.listA.getInterestPoints() == null )
+			this.listA.loadInterestPoints();
+
+		if ( this.listB.getInterestPoints() == null )
+			this.listB.loadInterestPoints();
+
+		for ( final InterestPoint i : this.listA.getInterestPoints() )
 			listA.add( new Detection( i.getId(), i.getL() ) );
 
-		for ( final InterestPoint i : pair.getListB() )
+		for ( final InterestPoint i : this.listB.getInterestPoints() )
 			listB.add( new Detection( i.getId(), i.getL() ) );
 
 		if ( listA.size() < 4 || listB.size() < 4 )
 		{
-			IOFunctions.println( "(" + new Date( System.currentTimeMillis() ) + "): " + comparison + ": Not enough detections to match" );
-			pair.setCandidates( new ArrayList< PointMatchGeneric< Detection > >() );
-			pair.setInliers( new ArrayList<PointMatchGeneric< Detection > >(), Double.NaN );
-			return pair;
+			result.result = "(" + new Date( System.currentTimeMillis() ) + "): " /* + comparison */ + ": Not enough detections to match";
+			result.setCandidates( new ArrayList< PointMatchGeneric< Detection > >() );
+			result.setInliers( new ArrayList<PointMatchGeneric< Detection > >(), Double.NaN );
+			return result;
 		}
 
 		final ArrayList< PointMatchGeneric< Detection > > candidates = hasher.extractCorrespondenceCandidates( 
@@ -70,17 +70,17 @@ public class GeometricHashingPairwise implements Callable< PairwiseMatch >
 				gp.getRatioOfDistance(), 
 				gp.getUseAssociatedBeads() );
 
-		pair.setCandidates( candidates );
+		result.setCandidates( candidates );
 
 		// compute ransac and remove inconsistent candidates
 		final ArrayList< PointMatchGeneric< Detection > > inliers = new ArrayList< PointMatchGeneric< Detection > >();
 
-		final Pair< String, Double > result = RANSAC.computeRANSAC( candidates, inliers, this.model.getModel(), rp.getMaxEpsilon(), rp.getMinInlierRatio(), rp.getMinInlierFactor(), rp.getNumIterations() );
+		final Pair< String, Double > ransacResult = RANSAC.computeRANSAC( candidates, inliers, gp.getModel(), rp.getMaxEpsilon(), rp.getMinInlierRatio(), rp.getMinInlierFactor(), rp.getNumIterations() );
 
-		pair.setInliers( inliers, result.getB() );
+		result.setInliers( inliers, ransacResult.getB() );
 
-		IOFunctions.println( "(" + new Date( System.currentTimeMillis() ) + "): " + comparison + ": " + result.getA() );
+		result.result = "(" + new Date( System.currentTimeMillis() ) + "): " + /*comparison +*/ ": " + ransacResult.getA();
 
-		return pair;
+		return result;
 	}
 }
