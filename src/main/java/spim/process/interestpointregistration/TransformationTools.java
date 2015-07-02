f83cf528735d4315d31ab25acb85f80af259f4b7
Stephan Preibisch <stephan.preibisch@gmx.de>
2015-07-02 20:19:42 +0200

    implemented mapback option

--- src/main/java/spim/process/interestpointregistration/TransformationTools.java
+++ src/main/java/spim/process/interestpointregistration/TransformationTools.java
@@ -6,22 +6,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import mpicbg.models.Affine3D;
 import mpicbg.models.AffineModel3D;
+import mpicbg.models.Model;
+import mpicbg.models.Point;
+import mpicbg.models.PointMatch;
+import mpicbg.models.RigidModel3D;
 import mpicbg.models.Tile;
-import mpicbg.spim.data.SpimData;
 import mpicbg.spim.data.registration.ViewRegistration;
-import mpicbg.spim.data.registration.ViewRegistrations;
-import mpicbg.spim.data.sequence.SequenceDescription;
+import mpicbg.spim.data.registration.ViewTransform;
+import mpicbg.spim.data.registration.ViewTransformAffine;
+import mpicbg.spim.data.sequence.ViewDescription;
 import mpicbg.spim.data.sequence.ViewId;
+import mpicbg.spim.io.IOFunctions;
+import net.imglib2.Dimensions;
+import net.imglib2.realtransform.AffineGet;
 import net.imglib2.realtransform.AffineTransform3D;
 import simulation.imgloader.SimulatedBeadsImgLoader;
 import spim.fiji.ImgLib2Temp.Pair;
 import spim.fiji.spimdata.SpimData2;
-import spim.fiji.spimdata.boundingbox.BoundingBoxes;
 import spim.fiji.spimdata.interestpoints.InterestPoint;
 import spim.fiji.spimdata.interestpoints.InterestPointList;
 import spim.fiji.spimdata.interestpoints.ViewInterestPointLists;
-import spim.fiji.spimdata.interestpoints.ViewInterestPoints;
 import spim.headless.interestpointdetection.DoGParameters;
 import spim.headless.registration.PairwiseResult;
 import spim.headless.registration.RANSACParameters;
@@ -32,6 +38,75 @@ import spim.process.interestpointregistration.pairwise.PairwiseStrategyTools;
 
 public class TransformationTools
 {
+	public static < M extends Model< M > > AffineTransform3D computeMapBackModel(
+			final ViewDescription mapBackView,
+			final ViewRegistration mapBackViewRegistration,
+			final AffineModel3D computedModel,
+			final M mapBackModel )
+	{
+		if ( mapBackModel.getMinNumMatches() > 4 )
+		{
+			IOFunctions.println( "Cannot map back using a model that needs more than 4 points: " + mapBackModel.getClass().getSimpleName() );
+
+			return null;
+		}
+		else
+		{
+			IOFunctions.println( "Mapping back to reference frame using a " + mapBackModel.getClass().getSimpleName() );
+
+			final Dimensions size = mapBackView.getViewSetup().getSize(); //ViewSetupUtils.getSizeOrLoad( referenceTileSetup, mapBackView.getTimePoint(), imgLoader );
+			long w = size.dimension( 0 );
+			long h = size.dimension( 1 );
+
+			final double[][] p = new double[][]{
+					{ 0, 0, 0 },
+					{ w, 0, 0 },
+					{ 0, h, 0 },
+					{ w, h, 0 } };
+
+			// original coordinates == pa
+			final double[][] pa = new double[ 4 ][ 3 ];
+
+			// map coordinates to the actual input coordinates
+			for ( int i = 0; i < p.length; ++i )
+				mapBackViewRegistration.getModel().apply( p[ i ], pa[ i ] );
+
+			// transformed coordinates == pb
+			final double[][] pb = new double[ 4 ][ 3 ];
+
+			for ( int i = 0; i < p.length; ++i )
+				pb[ i ] = computedModel.apply( pa[ i ] );
+
+			// compute the model that maps pb >> pa
+			try
+			{
+				final ArrayList< PointMatch > pm = new ArrayList< PointMatch >();
+				
+				for ( int i = 0; i < p.length; ++i )
+					pm.add( new PointMatch( new Point( pb[ i ] ), new Point( pa[ i ] ) ) );
+				
+				mapBackModel.fit( pm );
+			} catch ( Exception e )
+			{
+				IOFunctions.println( "Could not compute model for mapping back: " + e );
+				e.printStackTrace();
+				return null;
+			}
+
+			final AffineTransform3D mapBack = new AffineTransform3D();
+			final double[][] m = new double[ 3 ][ 4 ];
+			((Affine3D<?>)mapBackModel).toMatrix( m );
+			
+			mapBack.set( m[0][0], m[0][1], m[0][2], + m[0][3],
+						m[1][0], m[1][1], m[1][2], m[1][3], 
+						m[2][0], m[2][1], m[2][2], m[2][3] );
+
+			IOFunctions.println( "Model for mapping back: " + mapBack + "\n" );
+
+			return mapBack;
+		}
+	}
+
 	/** call this method to load interestpoints and apply current transformation */
 	public static Map< ViewId, List< InterestPoint > > getAllTransformedInterestPoints(
 			final Collection< ViewId > viewIds,
@@ -89,9 +164,29 @@ public class TransformationTools
 		return transformedList;
 	}
 
-	public static void applyTransformation( final ViewRegistration vr, final AffineTransform3D m)
+	public static void storeTransformation(
+			final ViewRegistration vr,
+			final ViewId viewId,
+			final Tile< ? > tile,
+			final AffineGet mapBackModel,
+			final String modelDescription )
 	{
-		vr.getTransformList().
+		// TODO: we assume that M is an Affine3D, which is not necessarily true
+		final Affine3D< ? > tilemodel = (Affine3D< ? >)tile.getModel();
+		final double[][] m = new double[ 3 ][ 4 ];
+		tilemodel.toMatrix( m );
+		
+		final AffineTransform3D t = new AffineTransform3D();
+		t.set( m[0][0], m[0][1], m[0][2], m[0][3],
+			   m[1][0], m[1][1], m[1][2], m[1][3],
+			   m[2][0], m[2][1], m[2][2], m[2][3] );
+
+		if ( mapBackModel != null )
+			t.preConcatenate( mapBackModel );
+
+		final ViewTransform vt = new ViewTransformAffine( modelDescription, t );
+		vr.preconcatenateTransform( vt );
+		vr.updateModel();
 	}
 
 	public static void main( String[] args )
@@ -152,9 +247,22 @@ public class TransformationTools
 				GlobalOpt.compute( new AffineModel3D(), result, fixedViews, groupedViews );
 
 		// map-back model
+		final RigidModel3D mapBackModel = new RigidModel3D();
+		final AffineTransform3D mapBack = computeMapBackModel(
+				spimData.getSequenceDescription().getViewDescription( viewIds.get( 0 ) ),
+				transformations.get( viewIds.get( 0 ) ),
+				models.get( viewIds.get( 0 ) ).getModel(),
+				mapBackModel );
 
 		// pre-concatenate models to spimdata2 viewregistrations
+		for ( final ViewId viewId : models.keySet() )
+		{
+			final Tile< AffineModel3D > tile = models.get( viewId );
+			final ViewRegistration vr = transformations.get( viewId );
 
+			storeTransformation( vr, viewId, tile, mapBack, "AffineModel3D" );
+		}
 
+		// save XML?
 	}
 }
