7a7efd0c9f8859b19c60f0e44e2f29f65d43ce60
Stephan Preibisch <stephan.preibisch@gmx.de>
2015-07-03 15:35:01 +0200

    remove autosaving of interestpoints

--- src/main/java/spim/headless/interestpointdetection/InterestPointTools.java
+++ src/main/java/spim/headless/interestpointdetection/InterestPointTools.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import mpicbg.spim.data.sequence.ViewId;
-import mpicbg.spim.io.IOFunctions;
 import spim.fiji.spimdata.SpimData2;
 import spim.fiji.spimdata.interestpoints.InterestPoint;
 import spim.fiji.spimdata.interestpoints.InterestPointList;
@@ -30,31 +29,15 @@ public class InterestPointTools
 	}
 
     /**
-     * Add interest points. Does not save the interest points
-     *
-     * @param data the data
-     * @param label the label
-     * @param points the points
-     * @param parameters the parameters
-     * @return the true if successful
-     */
-    public static boolean addInterestPoints( final SpimData2 data, final String label, final HashMap< ViewId, List< InterestPoint > > points, final String parameters )
-    {
-        return addInterestPoints(data,label,points,parameters,false);
-    }
-
-
-    /**
      * Add interest points.
      *
      * @param data the data
      * @param label the label
      * @param points the points
      * @param parameters the parameters
-     * @param saveNewInterestPoints the save new interest points
      * @return the true if successful, false if interest points cannot be saved
      */
-    public static boolean addInterestPoints( final SpimData2 data, final String label, final HashMap< ViewId, List< InterestPoint > > points, final String parameters, boolean saveNewInterestPoints)
+    public static boolean addInterestPoints( final SpimData2 data, final String label, final HashMap< ViewId, List< InterestPoint > > points, final String parameters )
 	{
 		for ( final ViewId viewId : points.keySet() )
 		{
@@ -70,12 +53,6 @@ public class InterestPointTools
 				list.setParameters( "" );
 
 			list.setInterestPoints( points.get( viewId ) );
-
-            if (saveNewInterestPoints && !list.saveInterestPoints()) {
-                IOFunctions.println("Error saving interest point list: " + new File(list.getBaseDir(), list.getFile().toString() + list.getInterestPointsExt()));
-                return false;
-            }
-
 
             final ViewInterestPointLists vipl = data.getViewInterestPoints().getViewInterestPointLists( viewId );
 			vipl.addInterestPointList( label, list );
--- src/main/java/spim/process/fusion/weightedavg/ProcessFusion.java
+++ src/main/java/spim/process/fusion/weightedavg/ProcessFusion.java
@@ -1,33 +1,5 @@
 package spim.process.fusion.weightedavg;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import mpicbg.spim.data.sequence.Channel;
-import mpicbg.spim.data.sequence.ImgLoader;
-import mpicbg.spim.data.sequence.TimePoint;
-import mpicbg.spim.data.sequence.ViewDescription;
-import mpicbg.spim.data.sequence.ViewId;
-import mpicbg.spim.data.sequence.VoxelDimensions;
-import net.imglib2.FinalInterval;
-import net.imglib2.Interval;
-import net.imglib2.RandomAccessible;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.RealRandomAccessible;
-import net.imglib2.img.Img;
-import net.imglib2.interpolation.InterpolatorFactory;
-import net.imglib2.realtransform.AffineTransform3D;
-import net.imglib2.type.NativeType;
-import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.complex.ComplexFloatType;
-import net.imglib2.type.numeric.integer.UnsignedShortType;
-import net.imglib2.type.numeric.real.FloatType;
-import spim.fiji.spimdata.SpimData2;
-import spim.fiji.spimdata.ViewSetupUtils;
-import spim.process.fusion.boundingbox.BoundingBoxGUI;
-import spim.process.fusion.weights.Blending;
-import spim.process.fusion.weights.ContentBased;
-import bdv.img.hdf5.Hdf5ImageLoader;
 
 public abstract class ProcessFusion
 {
@@ -38,50 +10,4 @@ public abstract class ProcessFusion
 	public static double[] defaultContentBasedSigma1 = new double[]{ 20, 20, 20 };
 	public static double[] defaultContentBasedSigma2 = new double[]{ 40, 40, 40 };
 	public static boolean defaultAdjustContentBasedSigmaForAnisotropy = true;
-	
-	final protected SpimData2 spimData;
-	final List< ViewId > viewIdsToProcess;
-	final BoundingBoxGUI bb;
-	final boolean useBlending;
-	final boolean useContentBased;
-	
-	public ProcessFusion(
-			final SpimData2 spimData,
-			final List< ViewId > viewIdsToProcess,
-			final BoundingBoxGUI bb,
-			final boolean useBlending,
-			final boolean useContentBased  )
-	{
-		this.spimData = spimData;
-		this.viewIdsToProcess = viewIdsToProcess;
-		this.bb = bb;
-		this.useBlending = useBlending;
-		this.useContentBased = useContentBased;
-	}
-	
-	public abstract < T extends RealType< T > & NativeType< T > > Img< T > fuseStack(
-			final T type,
-			final InterpolatorFactory< T, RandomAccessible< T > > interpolatorFactory,
-			final TimePoint timepoint, 
-			final Channel channel );
-
-	
-	protected < T extends RealType< T > > ArrayList< RealRandomAccessible< FloatType > > getAllWeights(
-			final RandomAccessibleInterval< T > img,
-			final ViewDescription desc,
-			final ImgLoader< ? > imgLoader )
-	{
-		final ArrayList< RealRandomAccessible< FloatType > > weigheners = new ArrayList< RealRandomAccessible< FloatType > >();
-		
-		if ( useBlending )
-			weigheners.add( getBlending( new FinalInterval( img ), desc, imgLoader ) );
-
-		if ( useContentBased )
-			weigheners.add( getContentBased( img, desc, imgLoader ) );
-
-		return weigheners;
-	}
-
-
-
 }
