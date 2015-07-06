8d6935ecea419ac295dd63b26b5d9a05d04544e7
HongKee Moon <hkmoon@me.com>
2015-07-06 18:15:06 +0200

    Temporarily added resavehdf5 files

---
+++ src/main/java/spim/headless/resavehdf5/HeadlessParseQueryXML.java
@@ -0,0 +1,45 @@
+package spim.headless.resavehdf5;
+
+import mpicbg.spim.data.sequence.ImgLoader;
+import mpicbg.spim.data.sequence.SequenceDescription;
+import mpicbg.spim.data.sequence.ViewDescription;
+import mpicbg.spim.data.sequence.ViewSetup;
+import spim.fiji.plugin.queryXML.GenericLoadParseQueryXML;
+import spim.fiji.spimdata.SpimData2;
+import spim.fiji.spimdata.XmlIoSpimData2;
+
+/**
+ * Parse and load xml file
+ * For the cluster process, it uses clusterExtention.
+ */
+public class HeadlessParseQueryXML extends GenericLoadParseQueryXML<SpimData2, SequenceDescription, ViewSetup, ViewDescription, ImgLoader<?>, XmlIoSpimData2> {
+    public HeadlessParseQueryXML() {
+        super(new XmlIoSpimData2(""));
+    }
+
+    public boolean loadXML(final String xmlFilename, final boolean useCluster) {
+        // try to parse the file anyways
+        if (!tryParsing(xmlFilename, true))
+            return false;
+
+        // Process attribute choices
+//		for ( int i = 0; i < specifyAttributes.size(); ++i )
+//		{
+//			final String attribute = specifyAttributes.get( i );
+//			final int choice = gd.getNextChoiceIndex();
+//
+//			defaultAttributeChoice.put( attribute, choice );
+//			attributeChoice.put( attribute, choice );
+//		}
+
+        if (!queryDetails())
+            return false;
+
+        if (useCluster)
+            this.clusterExt = "job_" + createUniqueName();
+        else
+            this.clusterExt = "";
+
+        return true;
+    }
+}
