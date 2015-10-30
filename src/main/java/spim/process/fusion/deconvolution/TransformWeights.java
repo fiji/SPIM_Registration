80d3a647269277d756cc1c5af8a25e3dc440f886
Stephan Preibisch <stephan.preibisch@gmx.de>
2015-10-30 12:45:54 +0100

    added new class to only transform weights

--- src/main/java/spim/process/fusion/deconvolution/TransformWeights.java
+++ src/main/java/spim/process/fusion/deconvolution/TransformWeights.java
@@ -3,7 +3,6 @@ package spim.process.fusion.deconvolution;
 import java.util.concurrent.Callable;
 
 import net.imglib2.Cursor;
-import net.imglib2.Interval;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.RealRandomAccess;
 import net.imglib2.realtransform.AffineTransform3D;
@@ -17,76 +16,76 @@ import spim.process.fusion.weights.Blending;
  * 
  * @author Stephan Preibisch (stephan.preibisch@gmx.de)
  *
+ * @param <T>
  */
 public class TransformWeights implements Callable< String >
 {
 	final ImagePortion portion;
 	final AffineTransform3D transform;
-	final RandomAccessibleInterval< FloatType > blendingImg, overlapImg;
+	final RandomAccessibleInterval< FloatType > weightImg;
 	final Blending blending;
 	final int offsetX, offsetY, offsetZ, imgSizeX, imgSizeY, imgSizeZ;
 
 	public TransformWeights(
 			final ImagePortion portion,
-			final Interval imgInterval,
 			final Blending blending,
 			final AffineTransform3D transform,
-			final RandomAccessibleInterval< FloatType > overlapImg,
-			final RandomAccessibleInterval< FloatType > blendingImg,
+			final RandomAccessibleInterval< FloatType > weightImg,
 			final long[] offset )
 	{
 		this.portion = portion;
-		this.blendingImg = blendingImg;
+		this.weightImg = weightImg;
 		this.transform = transform;
-		this.overlapImg = overlapImg;
 		this.blending = blending;
 
 		this.offsetX = (int)offset[ 0 ];
 		this.offsetY = (int)offset[ 1 ];
 		this.offsetZ = (int)offset[ 2 ];
 
-		this.imgSizeX = (int)imgInterval.dimension( 0 );
-		this.imgSizeY = (int)imgInterval.dimension( 1 );
-		this.imgSizeZ = (int)imgInterval.dimension( 2 );
+		this.imgSizeX = (int)weightImg.dimension( 0 );
+		this.imgSizeY = (int)weightImg.dimension( 1 );
+		this.imgSizeZ = (int)weightImg.dimension( 2 );
 	}
-
+	
 	@Override
 	public String call() throws Exception 
 	{
-		// make the blending and get the transformations
+		// make the interpolators and get the transformations
 		final RealRandomAccess< FloatType > wr = blending.realRandomAccess();
 
-		final Cursor< FloatType > cursorO = Views.iterable( overlapImg ).localizingCursor();
-		final Cursor< FloatType > cursorB = Views.iterable( blendingImg ).cursor();
+		final Cursor< FloatType > cursorW = Views.iterable( weightImg ).cursor();
 
 		final float[] s = new float[ 3 ];
 		final float[] t = new float[ 3 ];
 
-		cursorO.jumpFwd( portion.getStartPosition() );
-		cursorB.jumpFwd( portion.getStartPosition() );
+		cursorW.jumpFwd( portion.getStartPosition() );
 
 		for ( int j = 0; j < portion.getLoopSize(); ++j )
-		{
-			// move img cursor forward any get the value (saves one access)
-			final FloatType o = cursorO.next();
-			cursorO.localize( s );
-			
-			// move weight cursor forward and get the value 
-			final FloatType b = cursorB.next();
+			loop( cursorW, wr, transform, s, t, offsetX, offsetY, offsetZ, imgSizeX, imgSizeY, imgSizeZ );
 
-			s[ 0 ] += offsetX;
-			s[ 1 ] += offsetY;
-			s[ 2 ] += offsetZ;
+		return portion + " finished successfully (transform input & precompute weights).";
+	}
 
-			transform.applyInverse( t, s );
+	private static final void loop(
+			final Cursor< FloatType > cursorW,
+			final RealRandomAccess< FloatType > wr,
+			final AffineTransform3D transform,
+			final float[] s, final float[] t,
+			final int offsetX, final int offsetY, final int offsetZ,
+			final int imgSizeX, final int imgSizeY, final int imgSizeZ )
+	{
+		// move weight cursor forward and get the value 
+		final FloatType w = cursorW.next();
+		cursorW.localize( s );
 
-			// compute weights in any part of the image (the border can be negative!)
-			wr.setPosition( t );
+		s[ 0 ] += offsetX;
+		s[ 1 ] += offsetY;
+		s[ 2 ] += offsetZ;
+		
+		transform.applyInverse( t, s );
 
-			o.set( o.get() + 1 );
-			b.set( wr.get() );
-		}
-
-		return portion + " finished successfully (visualize weights).";
+		// compute weights in any case (the border can be negative!)
+		wr.setPosition( t );
+		w.set( wr.get() );
 	}
 }
