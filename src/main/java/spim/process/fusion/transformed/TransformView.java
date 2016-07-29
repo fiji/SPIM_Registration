a8c6b1d7abd4c3e84ade132ecc5839d6b17220c0
Stephan Preibisch <stephan.preibisch@gmx.de>
2016-07-29 13:54:49 +0200

    more convenience-methods for displaying a transformed image

--- src/main/java/spim/process/fusion/transformed/TransformView.java
+++ src/main/java/spim/process/fusion/transformed/TransformView.java
@@ -5,7 +5,10 @@ import mpicbg.spim.data.registration.ViewRegistration;
 import mpicbg.spim.data.sequence.ImgLoader;
 import mpicbg.spim.data.sequence.ViewId;
 import net.imglib2.FinalInterval;
+import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.interpolation.InterpolatorFactory;
+import net.imglib2.outofbounds.OutOfBoundsFactory;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
@@ -79,4 +82,63 @@ public class TransformView
 		
 		return transformView( il, viewId, vr.getModel(), boundingBox, outsideValue, interpolation );
 	}
+
+	/**
+	 * Creates a virtual construct that transforms and zero-mins using a user-defined outofbounds & interpolation.
+	 * 
+	 * @param input - the input image
+	 * @param transform - the affine transformation
+	 * @param boundingBox - the bounding box (after transformation)
+	 * @param outOfBoundsFactory - outofboundsfactory
+	 * @param interpolatorFactory - any interpolatorfactory
+	 * @return
+	 */
+	public static < T extends RealType< T > > RandomAccessibleInterval< FloatType > transformView(
+			final RandomAccessibleInterval< T > input,
+			final AffineTransform3D transform,
+			final FinalInterval boundingBox,
+			final OutOfBoundsFactory< T, RandomAccessible< T > > outOfBoundsFactory,
+			final InterpolatorFactory< FloatType, RandomAccessible< FloatType > > interpolatorFactory )
+	{
+		final long[] offset = new long[ input.numDimensions() ];
+		final long[] size = new long[ input.numDimensions() ];
+
+		for ( int d = 0; d < offset.length; ++d )
+		{
+			offset[ d ] = boundingBox.min( d );
+			size[ d ] = boundingBox.dimension( d );
+		}
+
+		final TransformedInputGeneralRandomAccessible< T > virtual = new TransformedInputGeneralRandomAccessible< T >( input, transform, outOfBoundsFactory, interpolatorFactory, offset );
+
+		return Views.interval( virtual, new FinalInterval( size ) );
+	}
+
+	public static < T extends RealType< T > > RandomAccessibleInterval< FloatType > transformView(
+			final ImgLoader imgloader,
+			final ViewId viewId,
+			final AffineTransform3D transform,
+			final FinalInterval boundingBox,
+			final OutOfBoundsFactory< T, RandomAccessible< T > > outOfBoundsFactory,
+			final InterpolatorFactory< FloatType, RandomAccessible< FloatType > > interpolatorFactory )
+	{
+		final RandomAccessibleInterval inputImg = imgloader.getSetupImgLoader( viewId.getViewSetupId() ).getImage( viewId.getTimePointId() );
+
+		return transformView( inputImg, transform, boundingBox, outOfBoundsFactory, interpolatorFactory );
+	}
+
+	public static < T extends RealType< T > > RandomAccessibleInterval< FloatType > transformView(
+			final SpimData data,
+			final ViewId viewId,
+			final FinalInterval boundingBox,
+			final OutOfBoundsFactory< T, RandomAccessible< T > > outOfBoundsFactory,
+			final InterpolatorFactory< FloatType, RandomAccessible< FloatType > > interpolatorFactory )
+	{
+		final ImgLoader il = data.getSequenceDescription().getImgLoader();
+		final ViewRegistration vr = data.getViewRegistrations().getViewRegistration( viewId );
+		vr.updateModel();
+		
+		return transformView( il, viewId, vr.getModel(), boundingBox, outOfBoundsFactory, interpolatorFactory );
+	}
+
 }
---
+++ src/main/java/spim/process/fusion/transformed/TransformedInputGeneralRandomAccess.java
@@ -0,0 +1,184 @@
+package spim.process.fusion.transformed;
+
+import net.imglib2.AbstractLocalizableInt;
+import net.imglib2.Localizable;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessible;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.RealRandomAccess;
+import net.imglib2.converter.RealFloatConverter;
+import net.imglib2.converter.read.ConvertedRandomAccessible;
+import net.imglib2.interpolation.InterpolatorFactory;
+import net.imglib2.outofbounds.OutOfBoundsFactory;
+import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.view.Views;
+
+/**
+ * Virtually transforms any RandomAccessibleInterval<RealType> into a RandomAccess<FloatType> using an AffineTransformation
+ * 
+ * @author preibisch
+ *
+ * @param <T>
+ */
+public class TransformedInputGeneralRandomAccess< T extends RealType< T > > extends AbstractLocalizableInt implements RandomAccess< FloatType >
+{
+	final RandomAccessibleInterval< T > img;
+	final OutOfBoundsFactory< T, RandomAccessible< T > > outOfBoundsFactory;
+	final InterpolatorFactory< FloatType, RandomAccessible< FloatType > > interpolatorFactory;
+	final AffineTransform3D transform;
+	final long[] offset;
+	final float[] s, t;
+	final FloatType v;
+
+	final RealRandomAccess< FloatType > ir;
+	final int offsetX, offsetY, offsetZ;
+	final int imgMinX, imgMinY, imgMinZ;
+	final int imgMaxX, imgMaxY, imgMaxZ;
+
+	@SuppressWarnings("unchecked")
+	public TransformedInputGeneralRandomAccess(
+			final RandomAccessibleInterval< T > img, // from ImgLoader
+			final AffineTransform3D transform,
+			final InterpolatorFactory< FloatType, RandomAccessible< FloatType > > interpolatorFactory,
+			final OutOfBoundsFactory< T, RandomAccessible< T > > outOfBoundsFactory,
+			final long[] offset )
+	{
+		super( img.numDimensions() );
+
+		this.offsetX = (int)offset[ 0 ];
+		this.offsetY = (int)offset[ 1 ];
+		this.offsetZ = (int)offset[ 2 ];
+
+		this.imgMinX = (int)img.min( 0 );
+		this.imgMinY = (int)img.min( 1 );
+		this.imgMinZ = (int)img.min( 2 );
+
+		this.imgMaxX = (int)img.max( 0 );
+		this.imgMaxY = (int)img.max( 1 );
+		this.imgMaxZ = (int)img.max( 2 );
+
+		this.img = img;
+		this.outOfBoundsFactory = outOfBoundsFactory;
+		this.interpolatorFactory = interpolatorFactory;
+		this.transform = transform;
+		this.offset = offset;
+		this.s = new float[ n ];
+		this.t = new float[ n ];
+		this.v = new FloatType();
+
+		// extend input image and convert to floats
+		final RandomAccessible< FloatType > input;
+
+		if ( FloatType.class.isInstance( Views.iterable( img ).cursor().next() ) )
+		{
+			input = (RandomAccessible< FloatType >)Views.extend( img, outOfBoundsFactory );
+		}
+		else
+		{
+			input =
+					new ConvertedRandomAccessible< T, FloatType >(
+						Views.extend( img, outOfBoundsFactory ),
+						new RealFloatConverter< T >(),
+						new FloatType() );
+		}
+
+		// make the interpolator
+		this.ir = Views.interpolate( input, interpolatorFactory ).realRandomAccess();
+	}
+
+	@Override
+	public FloatType get()
+	{
+		// go from PSI(Decon)_image local coordinate system to world coordinate system
+		s[ 0 ] = position[ 0 ] + offsetX;
+		s[ 1 ] = position[ 1 ] + offsetY;
+		s[ 2 ] = position[ 2 ] + offsetZ;
+
+		// go from world coordinate system to local coordinate system of input image (pixel coordinates)
+		transform.applyInverse( t, s );
+
+		ir.setPosition( t );
+		return ir.get();
+	}
+
+	@Override
+	public TransformedInputGeneralRandomAccess< T > copy()
+	{
+		return copyRandomAccess();
+	}
+
+	@Override
+	public TransformedInputGeneralRandomAccess< T > copyRandomAccess()
+	{
+		final TransformedInputGeneralRandomAccess< T > r = new TransformedInputGeneralRandomAccess< T >( img, transform, interpolatorFactory, outOfBoundsFactory, offset );
+		r.setPosition( this );
+		return r;
+	}
+
+	@Override
+	public void fwd( final int d ) { ++position[ d ]; }
+
+	@Override
+	public void bck( final int d ) { --position[ d ]; }
+
+	@Override
+	public void move( final int distance, final int d ) { position[ d ] += distance; }
+
+	@Override
+	public void move( final long distance, final int d ) { position[ d ] += distance; }
+
+	@Override
+	public void move( final Localizable localizable )
+	{
+		for ( int d = 0; d < n; ++d )
+			position[ d ] += localizable.getIntPosition( d );
+	}
+
+	@Override
+	public void move( final int[] distance )
+	{
+		for ( int d = 0; d < n; ++d )
+			position[ d ] += distance[ d ];
+	}
+
+	@Override
+	public void move( final long[] distance )
+	{
+		for ( int d = 0; d < n; ++d )
+			position[ d ] += distance[ d ];
+	}
+
+	@Override
+	public void setPosition( final Localizable localizable )
+	{
+		localizable.localize( position );
+	}
+
+	@Override
+	public void setPosition( final int[] pos )
+	{
+		for ( int d = 0; d < n; ++d )
+			position[ d ] = pos[ d ];
+	}
+
+	@Override
+	public void setPosition( final long[] pos )
+	{
+		for ( int d = 0; d < n; ++d )
+			position[ d ] = ( int ) pos[ d ];
+	}
+
+	@Override
+	public void setPosition( final int pos, final int d )
+	{
+		position[ d ] = pos;
+	}
+
+	@Override
+	public void setPosition( final long pos, final int d )
+	{
+		position[ d ] = ( int ) pos;
+	}
+}
