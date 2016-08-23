d1564d9fa61f1b00e1c77dfa1e9563f9cddcbc84
RichardMyers <richard@intelligent-imaging.com>
2016-08-23 20:13:34 +0200

    undo accidental changes to files

--- src/main/java/spim/fiji/datasetmanager/LightSheetZ1.java
+++ src/main/java/spim/fiji/datasetmanager/LightSheetZ1.java
@@ -110,7 +110,8 @@ public class LightSheetZ1 implements MultiViewDatasetDefinition
 		// finally create the SpimData itself based on the sequence description and the view registration
 		final SpimData2 spimData = new SpimData2( new File( directory ), sequenceDescription, viewRegistrations, viewInterestPoints, new BoundingBoxes() );
 
-		Apply_Transformation.applyAxis( spimData );
+		if ( meta.applyAxis() )
+			Apply_Transformation.applyAxis( spimData );
 
 		return spimData;
 	}
@@ -285,6 +286,7 @@ public class LightSheetZ1 implements MultiViewDatasetDefinition
 
 		final boolean modifyCal = defaultModifyCal = gd.getNextBoolean();
 		final boolean modifyAxis = defaultRotAxis = gd.getNextBoolean();
+		meta.setApplyAxis( defaultApplyRotAxis = gd.getNextBoolean() );
 
 		if ( modifyAxis || modifyCal )
 		{
