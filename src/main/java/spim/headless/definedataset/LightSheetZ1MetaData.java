f9f4b604b6571620e785f4c227b2fa66724a87e4
Stephan Preibisch <stephan.preibisch@gmx.de>
2016-08-26 08:14:12 +0200

    fix channel names in Z1 Metadataparsing

--- src/main/java/spim/headless/definedataset/LightSheetZ1MetaData.java
+++ src/main/java/spim/headless/definedataset/LightSheetZ1MetaData.java
@@ -156,7 +156,7 @@ public class LightSheetZ1MetaData
 				return false;
 			}
 
-			//printMetaData( r );
+			printMetaData( r );
 		}
 		catch ( Exception e )
 		{
@@ -317,8 +317,9 @@ public class LightSheetZ1MetaData
 
 			for ( int c = 0; c < numC; ++c )
 			{
+				tmp = metaData.get( "Information|Image|Channel|IlluminationWavelength|SinglePeak #" + ( c+1 ) );
 				//tmp = metaData.get( "Information|Image|Channel|Wavelength #" + ( c+1 ) );
-				tmp = metaData.get( "Experiment|AcquisitionBlock|MultiTrackSetup|TrackSetup|Attenuator|Laser #" + ( c+1 ) );
+				//tmp = metaData.get( "Experiment|AcquisitionBlock|MultiTrackSetup|TrackSetup|Attenuator|Laser #" + ( c+1 ) );
 
 				channels[ c ] = (tmp != null) ? tmp.toString() : String.valueOf( c );
 
@@ -335,6 +336,12 @@ public class LightSheetZ1MetaData
 
 				if ( channels[ c ].length() == 0 )
 					channels[ c ] = String.valueOf( c );
+
+				try
+				{
+					channels[ c ] = Integer.toString( (int)Double.parseDouble( channels[ c ] ) );
+				}
+				catch ( NumberFormatException e ) {}
 			}
 		}
 		catch ( Exception e )
