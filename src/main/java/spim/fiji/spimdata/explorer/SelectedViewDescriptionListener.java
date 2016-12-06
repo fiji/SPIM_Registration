f3469c8b13baee9c7544c321e6559542a6d5bf9e
David Hörl <hoerldavid@gmail.com>
2016-12-06 15:03:30 +0100

    SelectedViewDescriptionListener now gets notified with all (List of Lists) selected VDs, not just first

--- src/main/java/spim/fiji/spimdata/explorer/SelectedViewDescriptionListener.java
+++ src/main/java/spim/fiji/spimdata/explorer/SelectedViewDescriptionListener.java
@@ -3,10 +3,12 @@ package spim.fiji.spimdata.explorer;
 import mpicbg.spim.data.generic.AbstractSpimData;
 import mpicbg.spim.data.generic.sequence.BasicViewDescription;
 import mpicbg.spim.data.generic.sequence.BasicViewSetup;
+import java.util.List;
 
 public interface SelectedViewDescriptionListener< AS extends AbstractSpimData< ? > >
 {
-	public void seletedViewDescription( BasicViewDescription< ? extends BasicViewSetup > viewDescription );
+	//public void firstSelectedViewDescriptions( List< BasicViewDescription< ? extends BasicViewSetup > > viewDescriptions );
+	public void selectedViewDescriptions( List< List< BasicViewDescription< ? extends BasicViewSetup > > > viewDescriptions );
 	public void updateContent( final AS data );
 	public void save();
 	public void quit();
