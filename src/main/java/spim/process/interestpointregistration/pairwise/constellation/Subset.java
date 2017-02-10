77ad91a1759c23d9b16bc9871a933da861e4490a
Stephan Preibisch <stephan.preibisch@gmx.de>
2017-02-10 09:41:59 +0100

    moved fixing method into the subset class

--- src/main/java/spim/process/interestpointregistration/pairwise/constellation/Subset.java
+++ src/main/java/spim/process/interestpointregistration/pairwise/constellation/Subset.java
@@ -1,5 +1,7 @@
 package spim.process.interestpointregistration.pairwise.constellation;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -43,4 +45,90 @@ public class Subset< V >
 	public Set< V > getContainedViews() { return containedViews; }
 	public Set< Set< V > > getGroups() { return groups; }
 	public Set< V > getFixedViews() { return fixedViews; }
+
+	public ArrayList< Pair< V, V > > fixViews( final List< V > fixedViews ) { return fixViews( this, fixedViews ); }
+
+	/**
+	 * Fix an additional list of views (removes them from pairs and subsets)
+	 * 
+	 * @param fixedViews
+	 */
+	public static < V > ArrayList< Pair< V, V > > fixViews(
+			final Subset< V > subset,
+			final List< V > fixedViews )
+	{
+		final ArrayList< Pair< V, V > > removed = new ArrayList<>();
+
+		// which of the fixed views are present in this subset?
+		final HashSet< V > fixedSubsetViews = new HashSet<>();
+
+		for ( final V fixedView : fixedViews )
+			if ( subset.getContainedViews().contains( fixedView ) )
+				fixedSubsetViews.add( fixedView );
+
+		// add those that might be there already
+		fixedSubsetViews.addAll( subset.getFixedViews() );
+		subset.setFixedViews( fixedSubsetViews );
+
+		// remove pairwise comparisons between two fixed views
+		for ( int i = subset.getPairs().size() - 1; i >= 0; --i )
+		{
+			final Pair< V, V > pair = subset.getPairs().get( i );
+
+			// remove a pair if both views are fixed
+			if ( fixedSubsetViews.contains( pair.getA() ) && fixedSubsetViews.contains( pair.getB() ) )
+			{
+				subset.getPairs().remove( i );
+				removed.add( pair );
+			}
+		}
+
+		// now check if any of the fixed views is part of a group
+		// if so, no checks between groups where each contains at 
+		// least one fixed tile are necessary
+		final ArrayList< Set< V > > groupsWithFixedViews = new ArrayList<>();
+
+		for ( final Set< V > group : subset.getGroups() )
+		{
+			for ( final V fixedView : fixedSubsetViews )
+				if ( group.contains( fixedView ) )
+				{
+					groupsWithFixedViews.add( group );
+					break;
+				}
+		}
+
+		// if there is more than one group containing fixed views,
+		// we need to remove all pairs between them
+		if ( groupsWithFixedViews.size() > 1 )
+		{
+			for ( int i = subset.getPairs().size() - 1; i >= 0; --i )
+			{
+				final Pair< V, V > pair = subset.getPairs().get( i );
+
+				final V a = pair.getA();
+				final V b = pair.getB();
+
+				// if a and b are present in any combination of fixed groups
+				// they do not need to be compared
+				boolean aPresent = false;
+				boolean bPresent = false;
+
+				for ( final Set< V > fixedGroup : groupsWithFixedViews )
+				{
+					aPresent |= fixedGroup.contains( a );
+					bPresent |= fixedGroup.contains( b );
+
+					if ( aPresent && bPresent )
+					{
+						subset.getPairs().remove( i );
+						removed.add( pair );
+						break;
+					}
+				}
+			}
+		}
+
+		return removed;
+	}
 }
