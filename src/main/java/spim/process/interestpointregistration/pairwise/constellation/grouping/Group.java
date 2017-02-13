8e004992629a358460f2ce0ed7f71404a51d360a
Stephan Preibisch <stephan.preibisch@gmx.de>
2017-02-13 08:17:16 +0100

    made Group implement hashCode() and equals() to be able to use them as key in HashMap

--- src/main/java/spim/process/interestpointregistration/pairwise/constellation/grouping/Group.java
+++ src/main/java/spim/process/interestpointregistration/pairwise/constellation/grouping/Group.java
@@ -27,4 +27,38 @@ public class Group< V > implements Iterable< V >
 
 	@Override
 	public Iterator< V > iterator() { return views.iterator(); }
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ( ( views == null ) ? 0 : views.hashCode() );
+		return result;
+	}
+
+	@Override
+	public boolean equals( Object obj )
+	{
+		if ( this == obj )
+			return true;
+		if ( obj == null )
+			return false;
+		if ( getClass() != obj.getClass() )
+			return false;
+
+		final Group< ? > other = (Group< ? >) obj;
+		if ( views == null )
+		{
+			if ( other.views != null )
+				return false;
+		}
+		else if ( !views.equals( other.views ) )
+		{
+			return false;
+		}
+
+		return true;
+	}
+
 }
