1a23c06628f5ca8de19887c76eaaf11920b7e060
Stephan Preibisch <stephan.preibisch@gmx.de>
2017-05-11 10:28:29 +0200

    added overlap test

--- src/main/java/spim/fiji/plugin/interestpointregistration/parameters/BasicRegistrationParameters.java
+++ src/main/java/spim/fiji/plugin/interestpointregistration/parameters/BasicRegistrationParameters.java
@@ -7,15 +7,21 @@ import spim.fiji.plugin.interestpointregistration.pairwise.PairwiseGUI;
 
 public class BasicRegistrationParameters
 {
-	public static String[] registrationTypes = {
+	public static String[] registrationTypeChoices = {
 			"Register timepoints individually", 
 			"Match against one reference timepoint (no global optimization)", 
 			"All-to-all timepoints matching (global optimization)", 
 			"All-to-all timepoints matching with range ('reasonable' global optimization)" };
 
+	public static String[] overlapChoices = {
+			"Compare all views against each other",
+			"Only compare overlapping views (according to current transformations)" };
+
 	public enum RegistrationType { TIMEPOINTS_INDIVIDUALLY, TO_REFERENCE_TIMEPOINT, ALL_TO_ALL, ALL_TO_ALL_WITH_RANGE };
+	public enum OverlapType { ALL_AGAINST_ALL, OVERLAPPING_ONLY };
 
 	public PairwiseGUI pwr;
 	public RegistrationType registrationType;
+	public OverlapType overlapType;
 	public HashMap< ViewId, String > labelMap;
 }
