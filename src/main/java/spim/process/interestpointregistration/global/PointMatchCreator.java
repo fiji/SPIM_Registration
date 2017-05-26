febd1ebe431467732f6730330a0b60393c0b4803
Stephan Preibisch <stephan.preibisch@gmx.de>
2017-05-26 10:21:59 +0200

    java-doc for pointmatchcreator interface

--- src/main/java/spim/process/interestpointregistration/global/PointMatchCreator.java
+++ src/main/java/spim/process/interestpointregistration/global/PointMatchCreator.java
@@ -11,7 +11,23 @@ import spim.process.interestpointregistration.pairwise.constellation.grouping.Gr
 
 public interface PointMatchCreator
 {
+	/**
+	 * @return - all views that this class knows and that are part of the global opt
+	 */
 	public HashSet< ViewId > getAllViews();
-	public < M extends Model< M > > void assignPointMatches( final HashMap< ViewId, Tile< M > > map );
+
+	/**
+	 * assign pointmatches for all views that this object knows and that are present in tileMap.keySet(), which comes from getAllViews() plus what is in the group definition of the globalopt
+	 * 
+	 * @param tileMap - the map from viewId to Tile
+	 */
+	public < M extends Model< M > > void assignPointMatches( final HashMap< ViewId, Tile< M > > tileMap );
+
+	/**
+	 * By default all weights are 1, if wanted one can adjust them here, otherwise simply return
+	 * 
+	 * @param groups - which groups exist
+	 * @param tileMap - the map from viewId to Tile
+	 */
 	public < M extends Model< M > > void assignWeights( final ArrayList< Group< ViewId > > groups, final HashMap< ViewId, Tile< M > > tileMap );
 }
