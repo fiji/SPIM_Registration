80bf90d07da1d8ac682d563aed5d877f03a680c2
Stephan Preibisch <stephan.preibisch@gmx.de>
2017-05-28 11:55:36 +0200

    potentially fixed the TwoRoundGlobalOpt, it does return now one single AffineTransform per view, which is: 1st round global opt(strong links) + averageMapBack + 2nd round global opt (weak links)

--- src/main/java/spim/process/interestpointregistration/global/GlobalOptTwoRound.java
+++ src/main/java/spim/process/interestpointregistration/global/GlobalOptTwoRound.java
@@ -7,10 +7,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import mpicbg.models.Affine3D;
 import mpicbg.models.Model;
 import mpicbg.models.Tile;
 import mpicbg.spim.data.sequence.ViewId;
 import net.imglib2.realtransform.AffineGet;
+import net.imglib2.realtransform.AffineTransform3D;
 import spim.process.interestpointregistration.global.convergence.ConvergenceStrategy;
 import spim.process.interestpointregistration.global.convergence.IterativeConvergenceStrategy;
 import spim.process.interestpointregistration.global.linkremoval.LinkRemovalStrategy;
@@ -21,7 +23,7 @@ import spim.process.interestpointregistration.pairwise.constellation.grouping.Gr
 
 public class GlobalOptTwoRound
 {
-	public static < M extends Model< M > > HashMap< ViewId, Tile< M > > compute(
+	public static < M extends Model< M > > HashMap< ViewId, AffineTransform3D > compute(
 			final M model,
 			final PointMatchCreator pmc,
 			final IterativeConvergenceStrategy ics,
@@ -60,8 +62,32 @@ public class GlobalOptTwoRound
 		// the models that were applied before running the second round
 		final Map< ViewId, AffineGet > relativeTransforms = wlpmc.getRelativeTransforms();
 
-		// TODO: combine models from 1st and 2nd round
-		return models2;
+		// the combination of models from:
+		// the first round of global opt (strong links) + averageMapBack + the second round of global opt (weak links)
+		final HashMap< ViewId, AffineTransform3D > finalRelativeModels = new HashMap<>();
+
+		for ( final ViewId viewId : models2.keySet() )
+			finalRelativeModels.put( viewId, combineTransforms( models2.get( viewId ), relativeTransforms.get( viewId ) ) );
+
+		return finalRelativeModels;
+	}
+
+	public static < M extends Model< M > > AffineTransform3D combineTransforms( Tile< M > tile, final AffineGet relativeTransform )
+	{
+		final Affine3D< ? > tilemodel = (Affine3D< ? >)tile.getModel();
+		final double[][] m = new double[ 3 ][ 4 ];
+		tilemodel.toMatrix( m );
+		
+		final AffineTransform3D secondRunTransform = new AffineTransform3D();
+		secondRunTransform.set(
+				m[0][0], m[0][1], m[0][2], m[0][3],
+				m[1][0], m[1][1], m[1][2], m[1][3],
+				m[2][0], m[2][1], m[2][2], m[2][3] );
+
+		// we concatenate, not pre-concatenate since the relativeTransform from the first round comes first
+		secondRunTransform.concatenate( relativeTransform );
+
+		return secondRunTransform;
 	}
 
 	public static Group< ViewId > assembleViews( final Set< Tile< ? > > set, final HashMap< ViewId, ? extends Tile< ? > > models )
