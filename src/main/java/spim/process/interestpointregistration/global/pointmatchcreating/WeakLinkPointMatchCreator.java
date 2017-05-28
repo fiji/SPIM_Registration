8effbf10b1268a1b95a810b51a784afc9f1210be
Stephan Preibisch <stephan.preibisch@gmx.de>
2017-05-28 11:43:08 +0200

    the weak link creator can return an affine transformation for each view that was applied to achieve the coordinate transformations prior to the second run of global opt

--- src/main/java/spim/process/interestpointregistration/global/pointmatchcreating/WeakLinkPointMatchCreator.java
+++ src/main/java/spim/process/interestpointregistration/global/pointmatchcreating/WeakLinkPointMatchCreator.java
@@ -2,10 +2,12 @@ package spim.process.interestpointregistration.global.pointmatchcreating;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import mpicbg.models.Model;
 import mpicbg.models.Tile;
 import mpicbg.spim.data.sequence.ViewId;
+import net.imglib2.realtransform.AffineGet;
 import spim.process.interestpointregistration.pairwise.constellation.grouping.Group;
 
 public abstract class WeakLinkPointMatchCreator< M extends Model< M > > implements PointMatchCreator
@@ -18,4 +20,9 @@ public abstract class WeakLinkPointMatchCreator< M extends Model< M > > implemen
 		this.groupsNew = groupsNew;
 		this.models = models;
 	}
+
+	/**
+	 * @return - which transformation(s) have been applied before running the second round of global optimization
+	 */
+	public abstract Map< ViewId, AffineGet > getRelativeTransforms();
 }
