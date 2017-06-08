b1487a7347d906f30892c45d2b0e517604fa4205
Stephan Preibisch <stephan.preibisch@gmx.de>
2017-06-08 07:20:24 +0200

    test for caching and precomputing of images

--- src/main/java/spim/headless/deconvolution/TestDeconvolution.java
+++ src/main/java/spim/headless/deconvolution/TestDeconvolution.java
@@ -1,17 +1,20 @@
 package spim.headless.deconvolution;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import ij.ImageJ;
 import mpicbg.spim.data.SpimDataException;
 import mpicbg.spim.data.sequence.ViewId;
+import mpicbg.spim.io.IOFunctions;
 import net.imglib2.FinalInterval;
 import simulation.imgloader.SimulatedBeadsImgLoader;
 import spim.fiji.spimdata.SpimData2;
 import spim.fiji.spimdata.XmlIoSpimData2;
 import spim.fiji.spimdata.boundingbox.BoundingBox;
 import spim.process.deconvolution.ProcessInputImages;
+import spim.process.deconvolution.ProcessInputImages.ImgDataType;
 import spim.process.fusion.export.DisplayImage;
 import spim.process.fusion.transformed.FusedRandomAccessibleInterval;
 import spim.process.fusion.transformed.FusedWeightsRandomAccessibleInterval;
@@ -65,10 +68,15 @@ public class TestDeconvolution
 				spimData,
 				Group.toGroup( viewIds ),
 				boundingBox,
-				4.0 );
+				2.0 );
 
+		IOFunctions.println( "(" + new Date(System.currentTimeMillis()) + "): Virtual Fusion of groups " );
 		fusion.fuseGroups();
 
+		IOFunctions.println( "(" + new Date(System.currentTimeMillis()) + "): De-virtualization ... " );
+		fusion.deVirtualizeImages( ImgDataType.PRECOMPUTED, ImgDataType.CACHED );
+
+		IOFunctions.println( "(" + new Date(System.currentTimeMillis()) + "): Displaying " );
 		displayDebug( fusion );
 	}
 
@@ -78,19 +86,25 @@ public class TestDeconvolution
 
 		for ( final Group< V > group : fusion.getImgWeights().keySet() )
 		{
+			System.out.println( "Img Instance: " + fusion.getImgWeights().get( group ).getA().getClass().getSimpleName() );
+			System.out.println( "Weight Instance: " + fusion.getImgWeights().get( group ).getB().getClass().getSimpleName() );
+
 			DisplayImage.getImagePlusInstance( fusion.getImgWeights().get( group ).getA(), true, "g=" + i + " image", 0, 255 ).show();
-			DisplayImage.getImagePlusInstance( fusion.getImgWeights().get( group ).getB(), true, "g=" + i + " weightsDecon", 0, 1 ).show();
+			DisplayImage.getImagePlusInstance( fusion.getImgWeights().get( group ).getB(), true, "g=" + i + " weightsDecon", 0, 2 ).show();
 
-			final long[] dim = new long[ fusion.getDownsampledBoundingBox().numDimensions() ];
-			fusion.getDownsampledBoundingBox().dimensions( dim );
-
-			DisplayImage.getImagePlusInstance(
-					new FusedWeightsRandomAccessibleInterval(
-							new FinalInterval( dim ),
-							((FusedRandomAccessibleInterval)fusion.getImgWeights().get( group ).getA()).getWeights() ),
-					true,
-					"g=" + i + " weightsFusion",
-					0, 1 ).show();
+			if ( FusedRandomAccessibleInterval.class.isInstance( fusion.getImgWeights().get( group ).getA() ) )
+			{
+				final long[] dim = new long[ fusion.getDownsampledBoundingBox().numDimensions() ];
+				fusion.getDownsampledBoundingBox().dimensions( dim );
+	
+				DisplayImage.getImagePlusInstance(
+						new FusedWeightsRandomAccessibleInterval(
+								new FinalInterval( dim ),
+								((FusedRandomAccessibleInterval)fusion.getImgWeights().get( group ).getA()).getWeights() ),
+						true,
+						"g=" + i + " weightsFusion",
+						0, 1 ).show();
+			}
 
 			++i;
 		}
