7b5df00010b6cb8cea3da07948e9c9bf10351149
Stephan Preibisch <stephan.preibisch@gmx.de>
2017-06-17 19:10:03 +0200

    make Ransac Parameters adjustable

--- src/main/java/spim/process/interestpointregistration/pairwise/methods/ransac/RANSACParameters.java
+++ src/main/java/spim/process/interestpointregistration/pairwise/methods/ransac/RANSACParameters.java
@@ -7,32 +7,37 @@ package spim.process.interestpointregistration.pairwise.methods.ransac;
  */
 public class RANSACParameters
 {
-    public static float max_epsilon = 5;
-    public static float min_inlier_ratio = 0.1f;
-    public static int num_iterations = 1000;
-    public static float min_inlier_factor = 3f;
+	public static float max_epsilon = 5;
+	public static float min_inlier_ratio = 0.1f;
+	public static int num_iterations = 1000;
+	public static float min_inlier_factor = 3f;
+	
+	protected float maxEpsilon, minInlierRatio, minInlierFactor;
+	protected int numIterations;
 
-    final protected float maxEpsilon, minInlierRatio, minInlierFactor;
-    final protected int numIterations;
-    
-    public RANSACParameters( final float maxEpsilon, final float minInlierRatio, final float minInlierFactor, final int numIterations )
-    {
-    	this.maxEpsilon = maxEpsilon;
-    	this.minInlierRatio = minInlierRatio;
-    	this.minInlierFactor = minInlierFactor;
-    	this.numIterations = numIterations;
-    }
-    
-    public RANSACParameters()
-    {
-    	this.maxEpsilon = max_epsilon;
-    	this.numIterations = num_iterations;
-    	this.minInlierRatio = min_inlier_ratio;
-    	this.minInlierFactor = min_inlier_factor;
-    }
-    
-    public float getMaxEpsilon() { return maxEpsilon; }
-    public float getMinInlierRatio() { return minInlierRatio; }
-    public float getMinInlierFactor() { return minInlierFactor; }
-    public int getNumIterations() { return numIterations; }
+	public RANSACParameters( final float maxEpsilon, final float minInlierRatio, final float minInlierFactor, final int numIterations )
+	{
+		this.maxEpsilon = maxEpsilon;
+		this.minInlierRatio = minInlierRatio;
+		this.minInlierFactor = minInlierFactor;
+		this.numIterations = numIterations;
+	}
+
+	public RANSACParameters()
+	{
+		this.maxEpsilon = max_epsilon;
+		this.numIterations = num_iterations;
+		this.minInlierRatio = min_inlier_ratio;
+		this.minInlierFactor = min_inlier_factor;
+	}
+
+	public float getMaxEpsilon() { return maxEpsilon; }
+	public float getMinInlierRatio() { return minInlierRatio; }
+	public float getMinInlierFactor() { return minInlierFactor; }
+	public int getNumIterations() { return numIterations; }
+
+	public RANSACParameters setMaxEpsilon( final float maxEpsilon ) { this.maxEpsilon = maxEpsilon; return this; }
+	public RANSACParameters setMinInlierRatio( final float minInlierRatio ) { this.minInlierRatio = minInlierRatio; return this;  }
+	public RANSACParameters setMinInlierFactor( final float minInlierFactor ) { this.minInlierFactor = minInlierFactor; return this;  }
+	public RANSACParameters setNumIterations( final int numIterations ) { this.numIterations = numIterations; return this;  }
 }
