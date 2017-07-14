b8e47a91edfe8ff93f5242ba0c442330dd178bb4
David Hörl <hoerldavid@gmail.com>
2017-07-14 12:25:23 +0200

    finished preliminary implementation of FileMapImgLoaderLOCI2

--- src/main/java/spim/fiji/spimdata/imgloaders/filemap2/FileMapImgLoaderLOCI2.java
+++ src/main/java/spim/fiji/spimdata/imgloaders/filemap2/FileMapImgLoaderLOCI2.java
@@ -6,24 +6,38 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import loci.formats.FormatTools;
 import loci.formats.IFormatReader;
 import loci.formats.ImageReader;
 import mpicbg.spim.data.generic.sequence.AbstractSequenceDescription;
 import mpicbg.spim.data.generic.sequence.BasicViewDescription;
+import mpicbg.spim.data.generic.sequence.BasicViewSetup;
 import mpicbg.spim.data.generic.sequence.ImgLoaderHint;
 import mpicbg.spim.data.generic.sequence.ImgLoaderHints;
 import mpicbg.spim.data.sequence.ImgLoader;
 import mpicbg.spim.data.sequence.SetupImgLoader;
 import mpicbg.spim.data.sequence.ViewId;
 import mpicbg.spim.data.sequence.VoxelDimensions;
+import net.imglib2.Cursor;
 import net.imglib2.Dimensions;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.exception.IncompatibleTypeException;
+import net.imglib2.img.Img;
 import net.imglib2.img.ImgFactory;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.img.cell.CellImgFactory;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.integer.ShortType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.type.numeric.integer.UnsignedIntType;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Pair;
+import net.imglib2.view.Views;
+import spim.fiji.spimdata.imgloaders.AbstractImgLoader;
 import spim.fiji.spimdata.imgloaders.LegacyFileMapImgLoaderLOCI;
 
 public class FileMapImgLoaderLOCI2 implements ImgLoader
@@ -99,14 +113,35 @@ public class FileMapImgLoaderLOCI2 implements ImgLoader
 			RandomAccessibleInterval< T > img = null;
 			try
 			{
-				img = new VirtualRAIFactoryLOCI().createVirtualCached( reader, imageSource.getA(), imageSource.getB().getA(), imageSource.getB().getB(), allTimepointsInSingleFiles ? 0 : timepointId );
+				img = new VirtualRAIFactoryLOCI().createVirtualCached( reader, imageSource.getA(), imageSource.getB().getA(), imageSource.getB().getB(), allTimepointsInSingleFiles ? 0 : timepointId, null );
 			}
 			catch ( IncompatibleTypeException e )
 			{
 				e.printStackTrace();
 			}
 
-			// TODO: respect load completely
+			boolean loadCompletelyRequested = false;
+			for (ImgLoaderHint hint : hints)
+				if (hint == ImgLoaderHints.LOAD_COMPLETELY)
+					loadCompletelyRequested = true;
+
+			if (loadCompletelyRequested)
+			{
+				long numPx = 1;
+				for (int d = 0; d < img.numDimensions(); d++)
+					numPx *= img.dimension( d );
+				
+				final ImgFactory< T > imgFactory;
+				if (Math.log(numPx) / Math.log( 2 ) < 31)
+					imgFactory = new ArrayImgFactory<T>();
+				else
+					imgFactory = new CellImgFactory<T>();
+				
+				Img< T > loadedImg = imgFactory.create( img, getImageType() );
+				copy(Views.extendZero( img ), loadedImg);
+				
+				img = loadedImg;
+			}
 
 			return img;
 		}
@@ -114,7 +149,21 @@ public class FileMapImgLoaderLOCI2 implements ImgLoader
 		@Override
 		public T getImageType()
 		{
-			// TODO Auto-generated method stub
+			final BasicViewDescription< ? > aVd = getAnyPresentViewDescriptionForViewSetup( sd, setupId );
+			final Pair< File, Pair< Integer, Integer > > aPair = fileMap.get( aVd );
+
+			VirtualRAIFactoryLOCI.setReaderFileAndSeriesIfNecessary( reader, aPair.getA(), aPair.getB().getA() );
+			
+			if (reader.getPixelType() == FormatTools.UINT8)
+				return (T) new UnsignedByteType();
+			else if (reader.getPixelType() == FormatTools.UINT16)
+				return (T) new UnsignedShortType();
+			else if (reader.getPixelType() == FormatTools.INT16)
+				return (T) new ShortType();
+			else if (reader.getPixelType() == FormatTools.UINT32)
+				return (T) new UnsignedIntType();
+			else if (reader.getPixelType() == FormatTools.FLOAT)
+				return (T) new FloatType();
 			return null;
 		}
 
@@ -123,25 +172,98 @@ public class FileMapImgLoaderLOCI2 implements ImgLoader
 				ImgLoaderHint... hints)
 		{
 
-			// TODO: enforce type in the factory?
+			final BasicViewDescription< ? > vd = sd.getViewDescriptions().get( new ViewId( timepointId, setupId ) );
+			final Pair< File, Pair< Integer, Integer > > imageSource = fileMap.get( vd );
 
-			return null;
+			// TODO: some logging here? (reading angle .. , tp .., ... from file ...)
+
+			RandomAccessibleInterval< FloatType > img = null;
+			try
+			{
+				img = new VirtualRAIFactoryLOCI().createVirtualCached( reader, imageSource.getA(), imageSource.getB().getA(), imageSource.getB().getB(), allTimepointsInSingleFiles ? 0 : timepointId, new FloatType() );
+			}
+			catch ( IncompatibleTypeException e )
+			{
+				e.printStackTrace();
+			}
+
+
+
+			boolean loadCompletelyRequested = false;
+			for (ImgLoaderHint hint : hints)
+				if (hint == ImgLoaderHints.LOAD_COMPLETELY)
+					loadCompletelyRequested = true;
+
+			// we need the whole image in memory if we want to normalize or load completely
+			if (normalize || loadCompletelyRequested)
+			{
+				long numPx = 1;
+				for (int d = 0; d < img.numDimensions(); d++)
+					numPx *= img.dimension( d );
+				
+				final ImgFactory< FloatType > imgFactory;
+				if (Math.log(numPx) / Math.log( 2 ) < 31)
+					imgFactory = new ArrayImgFactory<FloatType>();
+				else
+					imgFactory = new CellImgFactory<FloatType>();
+				
+				Img< FloatType > loadedImg = imgFactory.create( img, new FloatType() );
+				copy(Views.extendZero( img ), loadedImg);
+				
+				img = loadedImg;
+			}
+
+			if (normalize)
+				AbstractImgLoader.normalize( img );
+
+			return img;
 		}
-
-		// TODO: we should already have a dimensions map when creating the img loader -> make member and use that.
 
 		@Override
 		public Dimensions getImageSize(int timepointId)
 		{
-			return null;
+			// NB: in all current uses we should have size information in the sd
+			BasicViewDescription< ? > vd = sd.getViewDescriptions().get( new ViewId( timepointId, setupId ) );
+			return vd.getViewSetup().getSize();
 		}
 
 		@Override
 		public VoxelDimensions getVoxelSize(int timepointId)
 		{
-			return null;
+			// NB: in all current uses we should have size information in the sd
+			BasicViewDescription< ? > vd = sd.getViewDescriptions().get( new ViewId( timepointId, setupId ) );
+			return vd.getViewSetup().getVoxelSize();
 		}
 
+	}
+
+	/**
+	 * copy src to dest
+	 * @param src : source, will not be modified
+	 * @param dest : destiantion, will be modified
+	 */
+	public static <T extends RealType<T>, S extends RealType<S>> void copy(RandomAccessible< T > src, RandomAccessibleInterval< S > dest)
+	{
+		final Cursor< S > destCursor = Views.iterable( dest ).localizingCursor();
+		final RandomAccess< T > srcRA = src.randomAccess();
+
+		while (destCursor.hasNext())
+		{
+			destCursor.fwd();
+			srcRA.setPosition( destCursor );
+			destCursor.get().setReal( srcRA.get().getRealDouble() );
+		}
+		
+	}
+
+	public static BasicViewDescription< ? > getAnyPresentViewDescriptionForViewSetup(AbstractSequenceDescription< ?, ?, ? > sd, int viewSetupId)
+	{
+		for (final ViewId vid : sd.getViewDescriptions().keySet())
+			if (vid.getViewSetupId() == viewSetupId)
+				if (!sd.getMissingViews().getMissingViews().contains( vid ))
+					return sd.getViewDescriptions().get( vid );
+
+		return null;
 	}
 
 }
