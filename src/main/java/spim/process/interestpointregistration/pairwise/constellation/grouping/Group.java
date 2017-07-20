bf56f2423ea89672d9ce7f6f98c162871e9982ab
Stephan Preibisch <stephan.preibisch@gmx.de>
2017-07-20 16:04:35 +0200

    more general toString method for groups

--- src/main/java/spim/process/interestpointregistration/pairwise/constellation/grouping/Group.java
+++ src/main/java/spim/process/interestpointregistration/pairwise/constellation/grouping/Group.java
@@ -488,18 +488,23 @@ public class Group< V > implements Iterable< V >
 	public static String pvid( final ViewId viewId ) { return "tpId=" + viewId.getTimePointId() + " setupId=" + viewId.getViewSetupId(); }
 	public static String pvids( final ViewId viewId ) { return viewId.getTimePointId() + "-" + viewId.getViewSetupId(); }
 	@SuppressWarnings("unchecked")
-	public static String gvids( final Group< ? > group )
+	public static String gvids( final Collection< ? > group )
 	{
 		String groupS = "";
 
-		if ( ViewId.class.isInstance( group.getViews().iterator().next() ) )
-			for ( final ViewId a : getViewsSorted( (Set<ViewId>)(group.getViews()) ) )
+		if ( ViewId.class.isInstance( group.iterator().next() ) )
+			for ( final ViewId a : getViewsSorted( (Collection<ViewId>)(group) ) )
 				groupS += pvids( a ) + " ";
 		else
-			for ( final Object a : group.getViews() )
+			for ( final Object a : group )
 				groupS += a + " ";
 
 		return groupS.trim();
+	}
+
+	public static String gvids( final Group< ? > group )
+	{
+		return gvids( group.getViews() );
 	}
 
 	public static void main( String[] args )
