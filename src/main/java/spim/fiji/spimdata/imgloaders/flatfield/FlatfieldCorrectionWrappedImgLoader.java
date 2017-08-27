66bde7b692faf0f6643fde7410eb615e013f81e4
David Hörl <hoerldavid@gmail.com>
2017-08-27 19:48:04 +0200

    implement ImgLoader, getter and setter for cached

--- src/main/java/spim/fiji/spimdata/imgloaders/flatfield/FlatfieldCorrectionWrappedImgLoader.java
+++ src/main/java/spim/fiji/spimdata/imgloaders/flatfield/FlatfieldCorrectionWrappedImgLoader.java
@@ -5,11 +5,13 @@ import java.io.File;
 import mpicbg.spim.data.sequence.ImgLoader;
 import mpicbg.spim.data.sequence.ViewId;
 
-public interface FlatfieldCorrectionWrappedImgLoader<IL extends ImgLoader>
+public interface FlatfieldCorrectionWrappedImgLoader<IL extends ImgLoader> extends ImgLoader
 {
 	public IL getWrappedImgLoder();
 	public void setActive(boolean active);
 	public boolean isActive();
+	public void setCached(boolean cached);
+	public boolean isCached();
 	public void setBrightImage(ViewId vId, File imgFile);
 	public void setDarkImage(ViewId vId, File imgFile);
 }
