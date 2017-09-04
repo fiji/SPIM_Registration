f5e33e930669e83c9f1e3366be9d936eb75e30d7
David Hörl <hoerldavid@gmail.com>
2017-09-04 11:55:53 +0200

    added helper to move windows around screen

---
+++ src/main/java/spim/fiji/plugin/util/MultiWindowLayoutHelper.java
@@ -0,0 +1,54 @@
+package spim.fiji.plugin.util;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class MultiWindowLayoutHelper
+{
+
+	/**
+	 * move a component (such as a JFrame Window) to x and y, expressed as fractions of the screen width and height
+	 * of the screen the component is on. The top-left corner of the component is moved to the desired location.
+	 * @param component the component to move
+	 * @param x desired left position (fraction of screen width)
+	 * @param y desired top position (fraction of screen height)
+	 */
+	public static void moveToScreenFraction(Component component, double x, double y)
+	{
+		// ensure we stay within the screen
+		x = Math.min( Math.max( x, 0 ), 1 );
+		y = Math.min( Math.max( y, 0 ), 1 );
+
+		// get the bounds of the screen we are on (without insets := toolbars, etc.)
+		final Rectangle screenRect = component.getGraphicsConfiguration().getBounds();
+		final Insets screenInsets = Toolkit.getDefaultToolkit().getScreenInsets( component.getGraphicsConfiguration() );
+		final Rectangle boundsWithoutInsets = new Rectangle(
+				screenRect.x + screenInsets.left,
+				screenRect.y + screenInsets.top,
+				screenRect.width - screenInsets.left - screenInsets.right,
+				screenRect.height - screenInsets.top - screenInsets.bottom );
+
+		final Dimension compontenSize = component.getSize();
+
+		final int newX = (int) Math.min(boundsWithoutInsets.x + x * boundsWithoutInsets.width, (boundsWithoutInsets.x + boundsWithoutInsets.width) - compontenSize.getWidth());
+		final int newY = (int) Math.min(boundsWithoutInsets.y + y * boundsWithoutInsets.height, (boundsWithoutInsets.y + boundsWithoutInsets.height) - compontenSize.getHeight());
+
+		component.setLocation( newX, newY );
+	}
+
+	public static void main(String[] args)
+	{
+		final JFrame theFrame = new JFrame(GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0].getDefaultConfiguration());
+		theFrame.setSize( 100, 100 );
+		theFrame.pack();
+		theFrame.setVisible( true );
+
+		moveToScreenFraction(theFrame, 0.5, 0.0);
+	}
+}
