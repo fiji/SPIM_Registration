09449ff267f92055e4be6de5a95f30fc6c36f0b6
David Hörl <hoerldavid@gmail.com>
2017-09-04 10:58:19 +0200

    init store a simple hash in StitchingResults, to make sure underlying ViewRegistrations have not changed

--- src/main/java/spim/fiji/spimdata/stitchingresults/PairwiseStitchingResult.java
+++ src/main/java/spim/fiji/spimdata/stitchingresults/PairwiseStitchingResult.java
@@ -1,5 +1,6 @@
 package spim.fiji.spimdata.stitchingresults;
 
+import mpicbg.spim.data.registration.ViewRegistration;
 import net.imglib2.RealInterval;
 import net.imglib2.realtransform.AffineGet;
 import net.imglib2.realtransform.AffineTransform3D;
@@ -35,6 +36,7 @@ public class PairwiseStitchingResult <C extends Comparable< C >>
 	final private AffineTransform3D transform;
 	final private RealInterval boundingBox;
 	final private double r;
+	final private double hash;
 
 	/**
 	 * 
@@ -47,19 +49,39 @@ public class PairwiseStitchingResult <C extends Comparable< C >>
 			final Pair< Group<C>, Group<C> > pair,
 			final RealInterval boundingBox,
 			final AffineGet transform,
-			final double r )
+			final double r,
+			final double hash)
 	{
 		this.pair = pair;
 		this.boundingBox = boundingBox;
 		this.transform = new AffineTransform3D();
 		this.transform.set( transform.getRowPackedCopy() );
 		this.r = r;
+		this.hash = hash;
 	}
 
-
+	public double getHash() {return hash;}
 	public Pair< Group<C>, Group<C> > pair() { return pair; }
 	public AffineGet getTransform() { return transform; }
 	public double r() { return r; }
 	public AffineGet getInverseTransform(){ return transform.inverse(); }
 	public RealInterval getBoundingBox(){ return boundingBox; }
+
+	/**
+	 * get a very simple hash of two ViewRegistrations
+	 * @param vrA first ViewRegistration
+	 * @param vrB second ViewRegistration
+	 * @return a hash
+	 */
+	public static double calculateHash(final ViewRegistration vrA, final ViewRegistration vrB)
+	{
+		vrA.updateModel();
+		vrB.updateModel();
+		final double[] rowPackedCopyVrA = vrA.getModel().getRowPackedCopy();
+		final double[] rowPackedCopyVrB = vrB.getModel().getRowPackedCopy();
+		double hash = 0;
+		for (int i=0; i<rowPackedCopyVrA.length; i++)
+			hash += (rowPackedCopyVrA[i] + 13 * rowPackedCopyVrB[i]);
+		return hash;
+	}
 }
