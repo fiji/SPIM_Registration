17214af4f8b4606dbb1d0c849e1a1b43a56909fc
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2017-12-29 23:15:46 +0100

    Fixes compile errors due to update to JFreeChart v1.5.0.

--- src/main/java/spim/fiji/plugin/thinout/MouseListenerValue.java
+++ src/main/java/spim/fiji/plugin/thinout/MouseListenerValue.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -33,15 +33,15 @@ import org.jfree.chart.ChartMouseListener;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.plot.ValueMarker;
 import org.jfree.chart.plot.XYPlot;
-import org.jfree.ui.RectangleAnchor;
-import org.jfree.ui.TextAnchor;
+import org.jfree.chart.ui.RectangleAnchor;
+import org.jfree.chart.ui.TextAnchor;
 
 public class MouseListenerValue implements ChartMouseListener
 {
 	final ChartPanel panel;
 	ValueMarker valueMarker;
-	
-	
+
+
 	MouseListenerValue( final ChartPanel panel, final double startValue )
 	{
 		this.panel = panel;
@@ -58,7 +58,7 @@ public class MouseListenerValue implements ChartMouseListener
 		valueMarker.setLabelPaint( Color.BLUE );
 		valueMarker.setLabelAnchor( RectangleAnchor.TOP );
 		valueMarker.setLabelTextAnchor( TextAnchor.TOP_LEFT );
-		
+
 		return valueMarker;
 	}
 
@@ -68,13 +68,13 @@ public class MouseListenerValue implements ChartMouseListener
 		// left mouse click
 		if ( e.getTrigger().getButton() == MouseEvent.BUTTON1 )
 		{
-			double value = getChartXLocation( e.getTrigger().getPoint(), panel );
-			
+			final double value = getChartXLocation( e.getTrigger().getPoint(), panel );
+
 			valueMarker.setValue( value );
 			valueMarker.setLabel(  " Distance=" + value );
 		}
 	}
-	
+
 	public static int getChartXLocation( final Point point, final ChartPanel panel )
 	{
 		final Point2D p = panel.translateScreenToJava2D( point );
@@ -82,12 +82,12 @@ public class MouseListenerValue implements ChartMouseListener
 		final XYPlot plot = (XYPlot) panel.getChart().getPlot();
 		final double chartX = plot.getDomainAxis().java2DToValue( p.getX(), plotArea, plot.getDomainAxisEdge() );
 		//final double chartY = plot.getRangeAxis().java2DToValue( p.getY(), plotArea, plot.getRangeAxisEdge() );
-		
-		return (int)Math.round( chartX );			
+
+		return (int)Math.round( chartX );
 	}
 
 	@Override
-	public void chartMouseMoved( ChartMouseEvent e )
+	public void chartMouseMoved( final ChartMouseEvent e )
 	{
 	}
 }
